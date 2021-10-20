e44a040975e22ed50adf18f44772db87ba6aff50
profesorderedes <profesorderedes@gmail.com>
2021-10-20 20:42:22 +0200

    mié 20 oct 2021 20:42:22 CEST

---
+++ Tema2/src/Ejemplo04.java
@@ -0,0 +1,27 @@
+
+// Programa que te pregunta la edad, y te dice si puedes pasar
+// a la discoteca o no.
+
+import java.util.Scanner;
+
+public class Ejemplo04 {
+
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+
+		// Preguntar la edad.
+		System.out.println("¿Cuántos años tienes?");
+		int edad = entrada.nextInt();
+
+		// Si la edad es mayor o igual a 18, se puede pasar.
+		if(edad >= 18) {
+			System.out.println("Puedes pasar a la discoteca.");
+		} else {
+			System.out.println("Vete a casa.");
+		}
+		
+		System.out.println("Gracias por venir a nuestro local.");
+		
+	}
+}
---
+++ Tema2/src/Ejemplo05.java
@@ -0,0 +1,29 @@
+
+// Programa que pregunta un precio de producto y si es mayor 
+// de 50 euros, te dice que tienes un descuento.
+
+import java.util.Scanner;
+
+public class Ejemplo05 {
+
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+
+		// Preguntar el precio del producto.
+		System.out.print("Precio del producto: ");
+		float precio = entrada.nextFloat();
+
+		// Comprobamos si el precio supera los 50 euros y
+		// en tal caso, indicamos que tendrá descuento.
+		if (precio > 50) {
+			System.out.println("¡Tiene derecho a descuento!");
+		}else {
+			System.out.println("No tiene derecho a descuento.");
+		}
+
+		System.out.println("\nGracias por su compra.");
+
+	}
+
+}
