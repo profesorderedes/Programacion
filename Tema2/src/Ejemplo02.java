1511d2ea18e453eb1d25beca2327b380c865540d
profesorderedes <profesorderedes@gmail.com>
2021-10-20 20:19:13 +0200

    mié 20 oct 2021 20:19:13 CEST

--- Tema2/src/Ejemplo02.java
+++ Tema2/src/Ejemplo02.java
@@ -1,4 +1,27 @@
 
+// Programa que pregunta un precio de producto y si es mayor 
+// de 50 euros, te dice que tienes un descuento.
+
+import java.util.Scanner;
+
 public class Ejemplo02 {
+
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+
+		// Preguntar el precio del producto.
+		System.out.print("Precio del producto: ");
+		float precio = entrada.nextFloat();
+
+		// Comprobamos si el precio supera los 50 euros y
+		// en tal caso, indicamos que tendrá descuento.
+		if (precio > 50) {
+			System.out.println("¡Tiene derecho a descuento!");
+		}
+
+		System.out.println("\nGracias por su compra.");
+
+	}
 
 }
