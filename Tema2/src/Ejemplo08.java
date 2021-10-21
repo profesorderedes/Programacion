0aaca5c8329a2ba48aed8502e004a6133e64f7f2
profesorderedes <profesorderedes@gmail.com>
2021-10-21 16:33:55 +0200

    jue 21 oct 2021 16:33:54 CEST

--- Tema2/src/Ejemplo08.java
+++ Tema2/src/Ejemplo08.java
@@ -20,9 +20,17 @@ public class Ejemplo08 {
 		float precio = entrada.nextFloat();
 		
 		// En función del precio, indicaremos el descuento.
+		if(precio < 50) {
+			System.out.println("Descuento = 0");
+		}else if(precio <= 100) {
+			System.out.println("Descuento = 5%");
+		}else if(precio <= 200) {
+			System.out.println("Descuento = 10%");
+		}else {
+			System.out.println("Descuento = 15%");
+		}
 		
-		
-		
+		System.out.println("Fin");
 		
 	}
 
