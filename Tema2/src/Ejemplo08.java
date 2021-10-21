850cad0c69e8dac64c5a3e2fc30c466deece4e34
profesorderedes <profesorderedes@gmail.com>
2021-10-21 15:51:13 +0200

    jue 21 oct 2021 15:51:13 CEST

--- Tema2/src/Ejemplo08.java
+++ Tema2/src/Ejemplo08.java
@@ -3,15 +3,43 @@
 Programa que pregunta el precio de un producto.
 Si es menor de 50 euros, no tendrá descuento, si está entre 50 
 y 100 euros (ambos inclusive), tendrá un descuento del 5%.
-Si está entre 100 y 200 euros, tendrá un 10% de descuento.
-En otro caso, tendrá un 15%.
+Si está entre 100 y 200 euros (incluyendo 200), tendrá un 10% de 
+descuento. En otro caso, tendrá un 15%.
  */
+
+import java.util.Scanner;
+
 public class Ejemplo08 {
 	
 	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		// Pedimos el precio.
+		System.out.print("Precio: ");
+		float precio = entrada.nextFloat();
+		
+		// En función del precio, indicaremos el descuento.
+		
 		
 		
 		
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
