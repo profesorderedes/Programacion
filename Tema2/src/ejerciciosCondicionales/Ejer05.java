7b5d2cf73f3467ede8ce07c012d0245439562a7a
profesorderedes <profesorderedes@gmail.com>
2021-10-22 16:39:32 +0200

    vie 22 oct 2021 16:39:32 CEST

--- Tema2/src/ejerciciosCondicionales/Ejer05.java
+++ Tema2/src/ejerciciosCondicionales/Ejer05.java
@@ -11,17 +11,16 @@ public class Ejer05 {
 		Scanner entrada = new Scanner(System.in);
 
 		System.out.println("Escribe un mes del año: ");
-
-		float mes = entrada.nextFloat();
+		int mes = entrada.nextInt();
 
 		if (mes >= 1 && mes <= 12) {
 
 			System.out.println("Mes correcto");
 
-		} else
+		} else {
 
 			System.out.println("Mes no válido");
-
+		}
 	}
 
 }
