eadfad51283769875df6f424153bfa6b89d23ca3
profesorderedes <profesorderedes@gmail.com>
2021-10-26 18:42:57 +0200

    mar 26 oct 2021 18:42:57 CEST

--- Tema2/src/ejerciciosCondicionales/Ejer04.java
+++ Tema2/src/ejerciciosCondicionales/Ejer04.java
@@ -7,14 +7,15 @@ public class Ejer04 {
 	public static void main(String[] args) {
 
 		Scanner entrada = new Scanner(System.in);
-				
+
 		System.out.println("Contraseña:  ");
-		String nombre= entrada.nextLine();
-		
-		if(nombre.equals("1234") ) {        //Aqui pondriamos nombre == "Pablo", pero como es un String usamos el nombre.equals("Pablo")
+		String nombre = entrada.nextLine();
+
+		if (nombre.equals("1234")) { // Aqui pondriamos nombre == "Pablo", pero como es un String usamos el
+										// nombre.equals("Pablo")
 			System.out.println("Acceso concedido");
 		}
-		
+
 	}
 
 }
--- Tema2/src/ejerciciosCondicionales/Ejer06.java
+++ Tema2/src/ejerciciosCondicionales/Ejer06.java
@@ -5,20 +5,29 @@ import java.util.Scanner;
 public class Ejer06 {
 
 	public static void main(String[] args) {
+
 		Scanner entrada = new Scanner(System.in);
+
 		System.out.println("Dime el precio:");
 		float precio = entrada.nextFloat();
+
 		System.out.println("Dime las unidades:");
-		int num = entrada.nextInt();
-		float total = precio * num;
-		if (total > 75) {
-			total = total - total *15/100;
-			System.out.println("Tiene un descuento del 15% " + total);
-		} else  {
-			total = total - total*5/100;
-			System.out.println("Tiene un descuento del 5% " + total);
+		int unidades = entrada.nextInt();
+
+		float total = precio * unidades;
+		float descuento;
 		
+		if (total > 75) {
+			descuento = 15;
+		} else {
+			descuento = 5;
 		}
+		
+		total = total - total * descuento / 100;
+		System.out.println("Tiene un descuento del " 
+				+ descuento + "%. "
+				+ "Precio final: " + total + " €.");
+		
+		
 	}
 }
-
