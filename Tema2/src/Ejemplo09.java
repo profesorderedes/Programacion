f860298750c9d2f46f87d0a2a63a4d8b134fac7e
profesorderedes <profesorderedes@gmail.com>
2021-10-26 19:11:52 +0200

    mar 26 oct 2021 19:11:51 CEST

---
+++ Tema2/src/Ejemplo09.java
@@ -0,0 +1,34 @@
+
+import java.util.Scanner;
+
+public class Ejemplo09 {
+
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.print("Escribe el número de aula: ");
+		int aula = entrada.nextInt();
+
+		// aula 1 -> Primero de grado superior
+		// aula 2 -> Segundo de grado superior
+		// aula 3 -> Primero de grado medio
+		// aula 4 -> Segundo de grado medio
+
+		if (aula == 1) {
+			System.out.println("Primero de grado superior");
+		} else if (aula == 2) {
+			System.out.println("Segundo de grado superior");
+		} else if (aula == 3) {
+			System.out.println("Primero de grado medio");
+		} else if (aula == 4) {
+			System.out.println("Segundo de grado medio");
+		} else {
+			System.out.println("Esa aula no existe.");
+		}
+		
+		
+
+	}
+
+}
