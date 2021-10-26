9b6770c6d34b8171f5828d4d8bdc5ae48e9d0895
profesorderedes <profesorderedes@gmail.com>
2021-10-26 20:39:24 +0200

    mar 26 oct 2021 20:39:23 CEST

---
+++ Tema2/src/ejerciciosCondicionales/Ejercicio14.java
@@ -0,0 +1,57 @@
+package ejerciciosCondicionales;
+
+import java.util.Scanner;
+
+public class Ejercicio14 {
+
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.println("Dime un número del 1 al 12 y te diré el mes que es:");
+		int mes = entrada.nextInt();
+
+		switch (mes) {
+		case 1:
+			System.out.println("Enero");
+			break;
+		case 2:
+			System.out.println("Febrero");
+			break;
+		case 3:
+			System.out.println("Marzo");
+			break;
+		case 4:
+			System.out.println("Abril");
+			break;
+		case 5:
+			System.out.println("Mayo");
+			break;
+		case 6:
+			System.out.println("Junio");
+			break;
+		case 7:
+			System.out.println("Julio");
+			break;
+		case 8:
+			System.out.println("Agosto");
+			break;
+		case 9:
+			System.out.println("Septiembre");
+			break;
+		case 10:
+			System.out.println("Octubre");
+			break;
+		case 11:
+			System.out.println("Noviembre");
+			break;
+		case 12:
+			System.out.println("Diciembre");
+			break;
+		default:
+			System.out.println("Este mes no es válido");
+		}
+
+	}
+
+}
