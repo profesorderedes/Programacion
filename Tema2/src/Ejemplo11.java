fee5bd02979fbfc16f0a4cf8a4ac01e0a8620554
profesorderedes <profesorderedes@gmail.com>
2021-11-02 19:03:46 +0100

    mar 02 nov 2021 19:03:46 CET

---
+++ Tema2/src/Ejemplo11.java
@@ -0,0 +1,40 @@
+
+import java.util.Scanner;
+
+public class Ejemplo11{
+
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.println("Dime el precio:");
+		float precio = entrada.nextFloat();
+
+		System.out.println("Dime las unidades:");
+		int unidades = entrada.nextInt();
+
+		float total = precio * unidades;
+		float descuento;
+
+//		if (total > 75) {
+//			descuento = 15;
+//		} else {
+//			descuento = 5;
+//		}
+		
+		// Operador ternario equivalente al if de arriba.
+		descuento = total > 75 ? 15 : 5;
+
+		total = total - total * descuento / 100;
+		System.out.println("Tiene un descuento del " + descuento 
+				+ "%. " + "\nPrecio final: " + total + " €.");
+
+	}
+}
+
+
+
+
+
+
+
