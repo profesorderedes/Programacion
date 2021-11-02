4a72066be15e65eead181910cf3b4918e69c2ff3
profesorderedes <profesorderedes@gmail.com>
2021-11-02 18:48:39 +0100

    mar 02 nov 2021 18:48:39 CET

---
+++ Tema2/src/ejerciciosCondicionales/Ejercicio13.java
@@ -0,0 +1,40 @@
+package ejerciciosCondicionales;
+
+import java.util.Scanner;
+
+public class Ejercicio13 {
+
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.println("Dime un mes del 1 al 4 y tediré cual es: ");
+		int mes = entrada.nextInt();
+
+		if (mes >= 1 && mes <= 4) {
+
+			if (mes == 1) {
+
+				System.out.println("Enero.");
+
+			} else if (mes == 2) {
+
+				System.out.println("Febrero");
+
+			} else if (mes == 3) {
+
+				System.out.println("Marzo.");
+
+			} else if (mes == 4) {
+
+				System.out.println("Abril.");
+
+			} 
+			
+		} else {
+			System.out.println("Mes incorrecto.");
+		}
+
+	}
+
+}
