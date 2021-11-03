4b0792d39e292464db6b6cf19f013e652841e4f7
profesorderedes <profesorderedes@gmail.com>
2021-11-03 20:30:07 +0100

    mié 03 nov 2021 20:30:07 CET

---
+++ Tema2/src/Ejemplo15.java
@@ -0,0 +1,20 @@
+
+public class Ejemplo15 {
+	
+	public static void main(String[] args) {
+		
+		int contador = 1;
+		
+		while(contador <= 5) {
+			
+			System.out.println(contador + ") No por mucho madrugar amanece "
+					+ "más temprano.");
+			
+			contador = contador + 1;
+		}
+		
+		System.out.println("\nDe nada.");
+		
+	}
+
+}
---
+++ Tema2/src/Ejemplo16.java
@@ -0,0 +1,13 @@
+
+/*
+	Programa que muestre los números del 1 al 100.
+ */
+public class Ejemplo16 {
+	
+	public static void main(String[] args) {
+		
+		
+		
+	}
+	
+}
