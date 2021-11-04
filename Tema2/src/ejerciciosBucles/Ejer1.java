dcf27a5454d950c8be7754f79fa1bf3da9fe3ea7
profesorderedes <profesorderedes@gmail.com>
2021-11-04 15:23:01 +0100

    jue 04 nov 2021 15:23:01 CET

--- Tema2/src/ejerciciosBucles/Ejer1.java
+++ Tema2/src/ejerciciosBucles/Ejer1.java
@@ -7,11 +7,18 @@ public class Ejer1 {
 	public static void main(String[] args) {
 
 		Scanner entrada = new Scanner(System.in);
-		
+
 		System.out.println("Escribe un carácter: ");
 		String caracter = entrada.nextLine();
-		
-		
-		
+
+		int contador = 1;
+
+		while (contador <= 100) {
+
+			System.out.println(contador + " " + caracter);
+			contador = contador + 1;
+
+		}
+
 	}
 }
