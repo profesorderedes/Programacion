4709449c466c30887670a56feb4ff5c2a28069f3
profesorderedes <profesorderedes@gmail.com>
2021-11-04 16:41:22 +0100

    jue 04 nov 2021 16:41:22 CET

---
+++ Tema2/src/ejerciciosBucles/Ejer5.java
@@ -0,0 +1,24 @@
+package ejerciciosBucles;
+
+import java.util.Scanner;
+
+public class Ejer5 {
+	
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Escribe un número y te diré "
+				+ "su tabla de multiplicar:");
+		int num = entrada.nextInt();
+		
+		int contador = 1;
+		
+		while() {
+			
+		}
+		
+		
+	}
+
+}
