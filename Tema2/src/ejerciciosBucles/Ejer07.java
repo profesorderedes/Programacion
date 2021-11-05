60dbe27b16722748afb26e146482637cd8c0e8cd
profesorderedes <profesorderedes@gmail.com>
2021-11-05 16:22:26 +0100

    vie 05 nov 2021 16:22:26 CET

--- Tema2/src/ejerciciosBucles/Ejer07.java
+++ Tema2/src/ejerciciosBucles/Ejer07.java
@@ -6,17 +6,17 @@ import java.util.Scanner;
 public class Ejer07 {
 
 	public static void main(String[] args) {
-		
+
 		Scanner entrada = new Scanner(System.in);
-		int cont = 1;
-		int contador = 0;
-		
-		while (cont <= 50)  {
-			System.out.println(cont + ")"+ "Numero: " + contador);
-			cont = cont +1;
-			contador = contador + 2 ;
+		int contador = 1;
+		int par = 0;
+
+		while (contador <= 50) {
+			System.out.println(contador + ")" + "Numero: " + par);
+			contador = contador + 1;
+			par = par + 2;
 		}
-				
+
 	}
-	
+
 }
