61ac01fe4a867f388265ebe239b73a7bf1e98b56
profesorderedes <profesorderedes@gmail.com>
2021-11-05 16:42:04 +0100

    vie 05 nov 2021 16:42:04 CET

--- Tema2/src/ejerciciosBucles/Ejer08.java
+++ Tema2/src/ejerciciosBucles/Ejer08.java
@@ -3,13 +3,13 @@ public class Ejer08 {
 
     public static void main(String[] args) {
          
-        int contador = 1;
+        int impar = 1;
        
-        while(contador < 200) {
-            if(contador%2 != 0 ) {
-                System.out.println(contador);
-            }
-            contador = contador+1;
+        while(impar < 200) {
+        	
+        	System.out.println(impar);
+            impar = impar + 2;
+            
         }
 
     }
