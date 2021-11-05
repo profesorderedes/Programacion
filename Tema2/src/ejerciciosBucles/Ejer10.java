680c7002709dd72c26c9e83db96bdd66662b2ee9
profesorderedes <profesorderedes@gmail.com>
2021-11-05 17:02:07 +0100

    vie 05 nov 2021 17:02:07 CET

---
+++ Tema2/src/ejerciciosBucles/Ejer10.java
@@ -0,0 +1,27 @@
+package ejerciciosBucles;
+
+import java.util.Scanner;
+
+/*
+¿Cómo se escribe 30 en hexadecimal?
+    1) 1E
+    2) 2F
+    3) 33
+    4) Ninguno de los anteriores.
+ */
+public class Ejer10 {
+
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.println("¿Cómo se escribe 30 en hexadecimal?");
+		System.out.println("\t1) 1E");
+		System.out.println("\t2) 2F");
+		System.out.println("\t3) 33");
+		System.out.println("\t4) Ninguno de los anteriores.");
+
+		int respuesta = entrada.nextInt();
+
+	}
+}
