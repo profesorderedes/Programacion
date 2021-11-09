ee6b0cdd8a30e985f98e24eafc2af3b7aaddbcee
profesorderedes <profesorderedes@gmail.com>
2021-11-09 19:17:27 +0100

    mar 09 nov 2021 19:17:27 CET

---
+++ Tema2/src/Ejemplo18.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class Ejemplo18 {
+
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("¡¡A ver si adivinas el número "
+				+ "entre 1 y 10!!");
+		
+		System.out.println("Número? ");
+		int num = entrada.nextInt();
+		
+		
+		
+	}
+}
