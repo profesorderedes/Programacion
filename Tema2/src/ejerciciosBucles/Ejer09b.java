35219c9129b68bb92ef6098fefcf3041ff349201
profesorderedes <profesorderedes@gmail.com>
2021-11-09 18:51:02 +0100

    mar 09 nov 2021 18:51:02 CET

---
+++ Tema2/src/ejerciciosBucles/Ejer09b.java
@@ -0,0 +1,31 @@
+package ejerciciosBucles;
+
+import java.util.Scanner;
+
+public class Ejer09b {
+
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.println("Dame un número:");
+		int resto = entrada.nextInt();
+
+		System.out.println("Dame otro número:");
+		int num2 = entrada.nextInt();
+
+		boolean restar = true;
+		
+		while(restar == true) {
+			
+			
+			
+		}
+		
+		
+
+		System.out.println("\nEl resto vale " + resto);
+
+	}
+
+}
