56ed4c7b3d8ec4e7ceb7081a244ac261af01a33b
profesorderedes <profesorderedes@gmail.com>
2021-11-09 18:48:04 +0100

    mar 09 nov 2021 18:48:04 CET

--- Tema2/src/ejerciciosBucles/Ejer10.java
+++ Tema2/src/ejerciciosBucles/Ejer10.java
@@ -4,10 +4,12 @@ import java.util.Scanner;
 
 public class Ejer10 {
 	public static void main(String[] args) {
+		
 		Scanner entrada = new Scanner(System.in);
-		boolean semaforo = true;
+		
+		boolean preguntar = true;
 
-		while (semaforo) {
+		while (preguntar) {
 
 			System.out.println(
 					"¿Cómo se escribe 30 en hexadecimal?\n\n  1)  1E\n\n  2)  2F\n\n  3)  33\n\n  4)  Ninguno de los anteriores.");
@@ -15,12 +17,14 @@ public class Ejer10 {
 
 			if (opcion == 1) {
 				System.out.println("\n¡Felicidades, has acertado!");
-				semaforo = false;
+				preguntar = false;
 			} else if (opcion > 4 || opcion < 1) {
 				System.out.println("\nEsa opción no existe.\n");
 			} else {
-				System.out.println("\nEsa opción es incorrecta.\n");
+				System.out.println("\nHas fallado.\n");
+				preguntar = false;
 			}
+			
 		}
 
 	}
--- Tema2/src/ejerciciosBucles/Ejer10b.java
+++ Tema2/src/ejerciciosBucles/Ejer10b.java
@@ -7,13 +7,12 @@ public class Ejer10b {
 
 		Scanner entrada = new Scanner(System.in);
 
-		boolean semaforo = true;
-
 		System.out.println(
 				"¿Cómo se escribe 30 en hexadecimal?\n\n  1)  1E\n\n  2)  2F\n\n  3)  33\n\n  4)  Ninguno de los anteriores.");
 		int opcion = entrada.nextInt();
 
 		while (opcion < 1 || opcion > 4) {
+			System.out.println("¡Tienes que escribir un número entre 1 y 4!");
 			System.out.println(
 					"¿Cómo se escribe 30 en hexadecimal?\n\n  1)  1E\n\n  2)  2F\n\n  3)  33\n\n  4)  Ninguno de los anteriores.");
 			opcion = entrada.nextInt();
