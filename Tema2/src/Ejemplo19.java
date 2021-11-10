d445f033955fce7a87d398c46d816469b530cfdb
profesorderedes <profesorderedes@gmail.com>
2021-11-10 19:51:45 +0100

    mié 10 nov 2021 19:51:45 CET

---
+++ Tema2/src/Ejemplo19.java
@@ -0,0 +1,30 @@
+
+public class Ejemplo19 {
+
+	public static void main(String[] args) {
+
+		// Mostramos los números del 1 al 100.
+
+		// Versión con bucle while.
+//		int i = 1;
+//
+//		while (i <= 100) {
+//			System.out.println(i);
+//			i++;
+//		}
+
+		// Versión con bucle for.
+		// for(valor inicial; condición; actualización)
+
+		for (int i = 1; i <= 100; i++) {
+
+			System.out.println(i);
+
+		}
+		
+		
+		
+
+	}
+
+}
---
+++ Tema2/src/ejerciciosBucles/Ejer05c.java
@@ -0,0 +1,40 @@
+package ejerciciosBucles;
+
+// Versión usando for.
+
+import java.util.Scanner;
+
+public class Ejer05c {
+
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.println("Escribe un número y te diré " + "su tabla de multiplicar:");
+		int num = entrada.nextInt();
+
+//		int contador = 1;
+//
+//		while (contador <= 10) {
+//
+//			System.out.println(num + " x " + contador + " = " + num * contador);
+//			contador++;
+//
+//		}
+		
+		for()
+
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
