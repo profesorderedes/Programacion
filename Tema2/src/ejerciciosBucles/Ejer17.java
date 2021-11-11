0cf4c88113551a115d100e663f9806226354da9d
profesorderedes <profesorderedes@gmail.com>
2021-11-11 15:53:26 +0100

    jue 11 nov 2021 15:53:25 CET

---
+++ Tema2/src/ejerciciosBucles/Ejer17.java
@@ -0,0 +1,22 @@
+package ejerciciosBucles;
+
+public class Ejer17 {
+
+	public static void main(String[] args) {
+
+		for (int i = 1; i <= 10; i++) {
+
+			// Hacer un bucle de 1 a 10 que muestre la tabla
+			// del número i
+			System.out.println("\nTabla del " + i);
+			System.out.println("=================");
+
+			for (int j = 1; j <= 10; j++) {
+				System.out.println(i + " x " + j + " = " + i * j);
+			}
+
+		}
+
+	}
+
+}
