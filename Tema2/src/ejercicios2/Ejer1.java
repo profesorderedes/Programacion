e28be122858e7011aadd501f1ba1e44d463826d0
profesorderedes <profesorderedes@gmail.com>
2021-11-16 19:28:57 +0100

    mar 16 nov 2021 19:28:57 CET

---
+++ Tema2/src/ejercicios2/Ejer1.java
@@ -0,0 +1,18 @@
+package ejercicios2;
+
+public class Ejer1 {
+	
+	public static void main(String[] args) {
+		
+		int i = 5;
+		
+		while(i <= 1000) {
+			
+			System.out.println(i);
+			i = i + 5;
+			
+		}
+		
+	}
+
+}
