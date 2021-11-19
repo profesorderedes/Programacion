20c0c839388a5d9b4c6fe9a7c334e842e88fc6da
profesorderedes <profesorderedes@gmail.com>
2021-11-19 16:53:10 +0100

    vie 19 nov 2021 16:53:10 CET

---
+++ Tema3/src/Ejemplo01.java
@@ -0,0 +1,26 @@
+
+public class Ejemplo01 {
+
+	public static void main(String[] args) {
+
+		float precios[] = new float[5];
+		// También se puede:
+		// float[] precios = new float[5];
+
+		precios[0] = 25;
+		precios[1] = 23.5F;
+		precios[2] = 24.3F;
+		precios[3] = 26;
+
+		System.out.println("En la posición 0 se encuentra " + precios[0]);
+		System.out.println("En la posición 1 se encuentra " + precios[1]);
+
+		System.out.println("La suma de los dos primeros " 
+				+ "precios es " + (precios[0] + precios[1]));
+
+		System.out.println("\nSi no introducimos un valor en "
+				+ "una posición, valdrá " + precios[4]);
+		
+	}
+
+}
