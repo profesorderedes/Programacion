7f49009fb062143207230ea8995fcbd75667b6a4
profesorderedes <profesorderedes@gmail.com>
2021-11-23 19:30:16 +0100

    mar 23 nov 2021 19:30:16 CET

--- Tema3/src/ejercicios1/Ejer04.java
+++ Tema3/src/ejercicios1/Ejer04.java
@@ -3,7 +3,13 @@ package ejercicios1;
 public class Ejer04 {
 
 	public static void main(String[] args) {
-		String companyeros[] = { "Yiyo", "Oscar", "Adrian", "Luis" };
+		
+		String companyeros[] = new String[4];
+		
+		companyeros[0] = "Yiyo";
+		companyeros[1] = "Óscar";
+		companyeros[2] = "Lluis";
+		companyeros[3] = "Adrián";
 
 		for (int i = 0; i < companyeros.length; i++) {
 			System.out.println(companyeros[i]);
