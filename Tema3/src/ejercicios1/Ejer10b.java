d17d94dc1d82420335c34379ad7b8cc9e62b832d
profesorderedes <profesorderedes@gmail.com>
2021-11-26 17:05:18 +0100

    vie 26 nov 2021 17:05:18 CET

--- Tema3/src/ejercicios1/Ejer10b.java
+++ Tema3/src/ejercicios1/Ejer10b.java
@@ -2,32 +2,33 @@ package ejercicios1;
 
 import java.util.Scanner;
 
-public class Ejer10 {
+public class Ejer10b {
 
 	public static void main(String[] args) {
 
 		Scanner entrada = new Scanner(System.in);
 		float compra[] = new float[100];
 
-		for (int i = 0; i < compra.length; i++) {
+		System.out.print("Precio 1: ");
+		compra[0] = entrada.nextFloat();
+
+		int i = 1;
+		while (i < compra.length && compra[i - 1] != 0) {
 
 			System.out.print("Precio " + (i + 1) + ": ");
+
 			compra[i] = entrada.nextFloat();
 
-			if (compra[i] == 0) {
-				break;
-			}
+			i++;
 
 		}
 
 		System.out.println("\nProductos:");
-		for (int i = 0; i < compra.length; i++) {
+		i = 0;
+		while (i < compra.length && compra[i] != 0) {
 
-			if (compra[i] != 0) {
-				System.out.println("Producto " + (i + 1) + ": " + compra[i] + " €");
-			} else {
-				break;
-			}
+			System.out.println("Producto " + (i + 1) + ": " + compra[i] + " €");
+			i++;
 
 		}
 
