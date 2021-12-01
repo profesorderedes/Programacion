ab2ccdc6b6145db11b43f1b4bba9823db85fb77b
profesorderedes <profesorderedes@gmail.com>
2021-12-01 20:06:39 +0100

    mié 01 dic 2021 20:06:38 CET

---
+++ Tema3/src/ejercicios1/Ejer14c.java
@@ -0,0 +1,42 @@
+package ejercicios1;
+
+/*
+ * Programa que va obteniendo números desde el 2 en adelante
+ * y los muestra en pantalla sólo si son primos.
+ */
+public class Ejer14c {
+
+	public static void main(String[] args) {
+
+		boolean esPrimo = true;
+		
+		// candidato es el número que vamos a comprobar si es primo.
+		for(int candidato = 2; candidato <= 100; candidato++) {
+			
+			// Si candidato es primo, lo mostramos.
+			
+			
+		}
+
+//		for (int i = 2; i < num; i++) {
+//
+//			System.out.println(num + "/" + i + " da resto " 
+//					+ num % i);
+//			
+//			if (num % i == 0) {
+//				System.out.println("¡¡¡NO ES PRIMO!!!");
+//				esPrimo = false;
+//				break;
+//			}
+//
+//		}
+//
+//		if (esPrimo) {
+//			System.out.println(num + " es primo.");
+//		} else {
+//			System.out.println(num + " es compuesto.");
+//		}
+
+	}
+
+}
