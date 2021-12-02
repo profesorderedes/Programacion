6f533486aa5d5149b3f98b474342b003ef15985a
profesorderedes <profesorderedes@gmail.com>
2021-12-02 15:30:17 +0100

    jue 02 dic 2021 15:30:17 CET

--- Tema3/src/ejercicios1/Ejer14c.java
+++ Tema3/src/ejercicios1/Ejer14c.java
@@ -1,7 +1,7 @@
 package ejercicios1;
 
 /*
- * Programa que va obteniendo números desde el 2 en adelante
+ * Programa que va obteniendo números desde el 2 hasta el 100
  * y los muestra en pantalla sólo si son primos.
  */
 public class Ejer14c {
@@ -9,33 +9,36 @@ public class Ejer14c {
 	public static void main(String[] args) {
 
 		boolean esPrimo = true;
+
+		System.out.println("Lista de números primos");
+		System.out.println("=======================");
 		
 		// candidato es el número que vamos a comprobar si es primo.
-		for(int candidato = 2; candidato <= 100; candidato++) {
+		for (int candidato = 2; candidato <= 100; candidato++) {
+
+			// Para cada número candidato a ser primo, empezamos
+			// suponiendo que lo es.
+			esPrimo = true;
 			
 			// Si candidato es primo, lo mostramos.
-			
-			
-		}
+			for (int i = 2; i < candidato; i++) {
 
-//		for (int i = 2; i < num; i++) {
-//
-//			System.out.println(num + "/" + i + " da resto " 
-//					+ num % i);
-//			
-//			if (num % i == 0) {
-//				System.out.println("¡¡¡NO ES PRIMO!!!");
-//				esPrimo = false;
-//				break;
-//			}
-//
-//		}
-//
-//		if (esPrimo) {
-//			System.out.println(num + " es primo.");
-//		} else {
-//			System.out.println(num + " es compuesto.");
-//		}
+//				System.out.println(candidato + "/" + i + " da resto " 
+//						+ candidato % i);
+
+				if (candidato % i == 0) {
+//					System.out.println("¡¡¡NO ES PRIMO!!!");
+					esPrimo = false;
+					break;
+				}
+
+			}
+
+			if (esPrimo) {
+				System.out.println(candidato);
+			}
+
+		}
 
 	}
 
