0f39c65ea6b42d8b694268a8b8742e6139bcadc6
profesorderedes <profesorderedes@gmail.com>
2021-12-03 16:22:53 +0100

    vie 03 dic 2021 16:22:53 CET

---
+++ Tema3/src/ejercicios1/Ejer15.java
@@ -0,0 +1,13 @@
+package ejercicios1;
+
+public class Ejer15 {
+
+	public static void main(String[] args) {
+
+		double temperaturas[] = { 15, 16.1, 16, 15.4, 15, 14, 
+				10.2, 11, 11.5, 10, 10.1, 9.8, 9, 6 };
+		
+		
+
+	}
+}
