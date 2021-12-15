51ae091399f9ac4313a12f1e6bbff1e1c4979927
profesorderedes <profesorderedes@gmail.com>
2021-12-15 20:07:11 +0100

    mié 15 dic 2021 20:07:11 CET

--- Tema3/src/Ejemplo07.java
+++ Tema3/src/Ejemplo07.java
@@ -9,37 +9,42 @@ public class Ejemplo07 {
 		int valores[] = { 51, 40, 42, 38, 23, 29 };
 		int posMin;
 		int aux;
-		
-		// Fase de búsqueda
-		
-		// Fase de intercambio
-		
+
 		for (int i = 0; i < valores.length; i++) {
-			
+
 			// El array sin ordenar empieza en la posición i.
-			
+
+			// Fase de búsqueda
 			// Buscamos el mínimo desde la posición i hasta el
 			// final del array.
 			posMin = i;
-			for(int j = i; j < valores.length; j++) {
-				
+			for (int j = i; j < valores.length; j++) {
+
 				// Si encontramos un valor más pequeño que
 				// el de posMin, actualizamos posMin a esa
 				// posición.
-				if(valores[i] < valores[posMin]) {
-					posMin = i;
+				if (valores[j] < valores[posMin]) {
+					posMin = j;
 				}
-				
+
 			}
-			
+
 			// Ahora la posición posMin contiene el valor más
 			// pequeño desde i hasta el final del array.
-			
-			
-			
+
+			// Fase de intercambio
+			// Intercambiamos el valor en posMin por el
+			// valor en i.
+			aux = valores[posMin];
+			valores[posMin] = valores[i];
+			valores[i] = aux;
+
 		}
 		
-		
+		for (int i = 0; i < valores.length; i++) {
+			System.out.print(valores[i] + " ");
+		}
+
 	}
 
 }
