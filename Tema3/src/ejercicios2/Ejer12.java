6f9d00d5c8abbc61cdeba28c5845fecb4ba16086
profesorderedes <profesorderedes@gmail.com>
2021-12-17 16:28:41 +0100

    vie 17 dic 2021 16:28:41 CET

---
+++ Tema3/src/ejercicios2/Ejer12.java
@@ -0,0 +1,44 @@
+package ejercicios2;
+
+public class Ejer12 {
+
+	public static void main(String[] args) {
+
+		int numeros[] = { 8, 2, 10, 20, 12, 71, 15, 23 };
+		int posMin;
+		int aux;
+
+		for (int i = 0; i < numeros.length - 1; i++) {
+
+			posMin = i;
+
+			for (int j = i + 1; j < numeros.length; j++) {
+
+				if (numeros[j] < numeros[posMin]) {
+					posMin = j;
+				}
+
+			}
+
+			aux = numeros[posMin];
+			numeros[posMin] = numeros[i];
+			numeros[i] = aux;
+
+		}
+
+		System.out.println(
+				"Los números atómicos ordenados de menor "
+				+ "a mayor:\n==================================="
+				+ "=============");
+
+		for (int i = 0; i < numeros.length; i++) {
+			System.out.print(numeros[i] + " ");
+		}
+
+		System.out.println("\nEl número atómico mas pequeño es " 
+				+ numeros[0] + " y el más grande es "
+				+ numeros[numeros.length - 1]);
+
+	}
+
+}
---
+++ Tema3/src/ejercicios2/Ejer12b.java
@@ -0,0 +1,45 @@
+package ejercicios2;
+
+public class Ejer12b {
+
+	public static void main(String[] args) {
+
+		int numeros[] = { 8, 2, 10, 20, 12, 71, 15, 23 };
+		int posMax;
+		int aux;
+
+		for (int i = 0; i < numeros.length - 1; i++) {
+
+			posMax = i;
+
+			for (int j = i + 1; j < numeros.length; j++) {
+
+				if (numeros[j] > numeros[posMax]) {
+					posMax = j;
+				}
+
+			}
+
+			aux = numeros[posMax];
+			numeros[posMax] = numeros[i];
+			numeros[i] = aux;
+
+		}
+
+		System.out.println(
+				"Los números atómicos ordenados de mayor a menor:"
+				+ "\n=========================================="
+				+ "======");
+
+		for (int i = 0; i < numeros.length; i++) {
+			System.out.print(numeros[i] + " ");
+		}
+
+		System.out.println("\nEl número atómico más pequeño es " 
+				+ numeros[numeros.length - 1] 
+				+ " y el más grande es "
+				+ numeros[0]);
+
+	}
+
+}
