5defb8e59d75550861098793f2746e5e06460b1f
profesorderedes <profesorderedes@gmail.com>
2021-12-17 16:58:12 +0100

    vie 17 dic 2021 16:58:12 CET

---
+++ Tema3/src/ejercicios2/Ejercicio13.java
@@ -0,0 +1,16 @@
+package ejercicios2;
+
+public class Ejercicio13 {
+
+	public static void main(String[] args) {
+
+		double medidas[][] = { 
+				{ 8.5,  1020, 40 }, 
+				{ 14.2, 1021, 35 }, 
+				{ 11,   1022, 45 }, 
+				{ 7,    1023, 50 } 
+		};
+
+	}
+
+}
