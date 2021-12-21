5d40d587f3783f8f1e68a83101f6c4702721d2c7
profesorderedes <profesorderedes@gmail.com>
2021-12-21 19:25:41 +0100

    mar 21 dic 2021 19:25:41 CET

--- Tema3/src/ejercicios2/Ejer14.java
+++ Tema3/src/ejercicios2/Ejer14.java
@@ -1,38 +1,45 @@
 package ejercicios2;
 
+import java.util.Iterator;
+
 public class Ejer14 {
-	
+
 	public static void main(String[] args) {
-		
-		String precios[][] = {
-				{"Tablet Samsung","120"},
-				{"Móvil Sony","115"},
-				{"Ipad Mini","290"},
-				{"Móvil HTC","95"}
-		};
-		
+
+		String precios[][] = { { "Tablet Samsung", "120" }, { "Móvil Sony", "115" }, { "Ipad Mini", "290" },
+				{ "Móvil HTC", "95" } };
+
 		System.out.println("La tabla tiene:");
 		System.out.println("\t- " + precios.length + " filas");
 		System.out.println("\t- " + precios[0].length + " columnas");
-		
+
 		// Recorremos cada fila de la tabla.
 		System.out.println("\nArtículos");
 		System.out.println("=========");
-		for(int fila = 0; fila < precios.length; fila++) {
-			
+		for (int fila = 0; fila < precios.length; fila++) {
+
 			// Recorremos cada columna de la tabla.
-			for(int col = 0; col < precios[0].length; col++) {
+			for (int col = 0; col < precios[0].length; col++) {
 				System.out.print(precios[fila][col] + "\t");
 			}
-			
+
 			System.out.println();
-			
+
 		}
+
+		// Sumamos todos los precios.
+		float total = 0;
+//		float precio;
+		for (int fila = 0; fila < precios.length; fila++) {
+
+//			precio = Float.parseFloat(precios[fila][1]);
+//			total = total + precio;
+			total = total + Float.parseFloat(precios[fila][1]);
+
+		}
+
+		System.out.println("\nEl total de artículos cuesta " + total + " euros.");
+
 	}
 
 }
-
-
-
-
-
