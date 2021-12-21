ee77a89dbbec56e25a50584a8d501461def5ba1a
profesorderedes <profesorderedes@gmail.com>
2021-12-21 18:46:56 +0100

    mar 21 dic 2021 18:46:55 CET

--- Tema3/src/ejercicios2/Ejercicio13.java
+++ Tema3/src/ejercicios2/Ejercicio13.java
@@ -5,11 +5,10 @@ public class Ejercicio13 {
 	public static void main(String[] args) {
 
 		double medidas[][] = { 
-				{ 8.5,  1020, 40 }, 
+				{ 8.5, 1020, 40 }, 
 				{ 14.2, 1021, 35 }, 
-				{ 11,   1022, 45 }, 
-				{ 7,    1023, 50 } 
-		};
+				{ 11, 1022, 45 }, 
+				{ 7, 1023, 50 } };
 
 		// Obtener el número de filas y columnas.
 		System.out.println("Tamaño de la tabla:");
@@ -18,21 +17,27 @@ public class Ejercicio13 {
 
 		// Mostrar el array.
 		System.out.println("\nContenido de la tabla:");
-		for(int fila = 0; fila < medidas.length; fila++) {
-			
+		for (int fila = 0; fila < medidas.length; fila++) {
+
 			System.out.println(medidas[fila][0] + "\t" 
-							+  medidas[fila][1] + "\t"
-							+  medidas[fila][2]);
-			
+					+ medidas[fila][1] + "\t" 
+					+ medidas[fila][2]);
+
 		}
-		
+
+		// Mostrar el array usando bucles anidados.
+		System.out.println("\nContenido de la tabla:");
+		for (int fila = 0; fila < medidas.length; fila++) {
+
+			for(int col = 0; col < medidas[0].length; col++) {
+				System.out.println(medidas[fila][col]);
+			}
+
+		}
+
 	}
 
 }
-
-
-
-
 
 
 
