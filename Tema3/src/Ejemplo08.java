384fa2715a5ef11d6b9d1d499550e842e8843564
profesorderedes <profesorderedes@gmail.com>
2022-01-11 19:07:57 +0100

    mar 11 ene 2022 19:07:57 CET

---
+++ Tema3/src/Ejemplo08.java
@@ -0,0 +1,28 @@
+
+public class Ejemplo08 {
+
+	public static void main(String[] args) {
+
+		String empleados[][] = { { "Vázquez del Valle", "Elier", "1300" }, { "Buendía Galindo", "Fernando", "1500" },
+				{ "Aguiló Pizá", "Jaume", "1800" }, { "González Torelló", "Guillermo", "2000" },
+				{ "Alfaro Esparcia", "María del Carmen", "1700" }, { "Losada Novo", "Iván", "1900" } };
+
+		// Buscar el mínimo y el máximo sueldo.
+		int posMin = 0;
+
+		for (int fila = 0; fila < empleados.length; fila++) {
+
+			if (Float.parseFloat(empleados[fila][2]) < Float.parseFloat(empleados[posMin][2])) {
+				posMin = fila;
+			}
+
+		}
+
+		System.out.println("El empleado que menos "
+				+ "gana es: " + empleados[posMin][1] 
+				+ " " + empleados[posMin][0] + " ("
+				+ empleados[posMin][2] + " euros)");
+
+	}
+
+}
