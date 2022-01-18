449c440f61021eb2a20fd3077dc1282eddb3c8d5
profesorderedes <profesorderedes@gmail.com>
2022-01-18 19:13:36 +0100

    mar 18 ene 2022 19:13:35 CET

--- Tema3/src/ejercicios3/Ejer5.java
+++ Tema3/src/ejercicios3/Ejer5.java
@@ -9,20 +9,19 @@ public class Ejer5 {
 		final float CONDICION_REBAJA = 50;
 
 		Scanner entrada = new Scanner(System.in);
-		float precios[] = new float[5];
+		float precios[] = new float[50];
 		float precio;
 		float suma = 0;
 
 		System.out.println("Caja registradora");
 		System.out.println("====================");
-		
+
 		// Leemos los precios.
 		for (int i = 0; i < precios.length; i++) {
 
 			System.out.print("Precio " + i + ": ");
 			precio = entrada.nextFloat();
 
-			
 			if (precio < 0) {
 				// Si el precio es negativo, hemos terminado.
 				break;
@@ -40,6 +39,21 @@ public class Ejer5 {
 			System.out.println("suma = " + suma);
 
 		}
+
+		// Mostramos los precios guardados en el array.
+		System.out.println("\nTiquet de la compra");
+		for (int i = 0; i < precios.length; i++) {
+
+			if (precios[i] == 0) {
+				break;
+			}
+
+			System.out.println(precios[i]);
+
+		}
+
+		System.out.println("\nTotal a pagar: " + suma + " €.");
+		System.out.println("Gracias por la compra.");
 
 	}
 
