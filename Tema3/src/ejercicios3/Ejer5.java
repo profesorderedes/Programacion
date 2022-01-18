cabbcda13c12ce4124c168de39b4e835e27fd39c
profesorderedes <profesorderedes@gmail.com>
2022-01-18 19:08:58 +0100

    mar 18 ene 2022 19:08:58 CET

--- Tema3/src/ejercicios3/Ejer5.java
+++ Tema3/src/ejercicios3/Ejer5.java
@@ -15,21 +15,28 @@ public class Ejer5 {
 
 		System.out.println("Caja registradora");
 		System.out.println("====================");
+		
+		// Leemos los precios.
 		for (int i = 0; i < precios.length; i++) {
 
 			System.out.print("Precio " + i + ": ");
 			precio = entrada.nextFloat();
 
+			
 			if (precio < 0) {
+				// Si el precio es negativo, hemos terminado.
 				break;
 			} else if (suma > CONDICION_REBAJA) {
+				// Si la suma acumulada pasa de 50 euros,
+				// guardamos el precio rebajado.
 				precios[i] = precio * 0.9f;
 				System.out.println("Artículo rebajado.");
 			} else {
+				// Por defecto, guardamos el precio tal cual.
 				precios[i] = precio;
 			}
 
-			suma = suma + precio;
+			suma = suma + precios[i];
 			System.out.println("suma = " + suma);
 
 		}
