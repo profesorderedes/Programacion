b1bfd6d15ea605ff78928377b2ab61a79e32b4f3
profesorderedes <profesorderedes@gmail.com>
2022-01-19 19:31:17 +0100

    mié 19 ene 2022 19:31:17 CET

---
+++ Tema3/src/ejercicios3/Ejer4.java
@@ -0,0 +1,26 @@
+package ejercicios3;
+
+public class Ejer4 {
+
+	public static void main(String[] args) {
+
+		double temperaturas[] = { 3.2, 3.5, 7.2, 0.1, 1, 
+				-2.3, -5, 3.4, 4, 4, 4, 1.4, 2.5, -0.4, -3.6 };
+		boolean positivo = true;
+
+		for (int i = 0; i < temperaturas.length; i++) {
+
+			if (temperaturas[i] < 0) {
+				System.out.println("Posición: " + i);
+				positivo = false;
+				break;
+			}
+
+		}
+		if (positivo == true) {
+			System.out.println("Resultado: -1");
+		}
+
+	}
+
+}
