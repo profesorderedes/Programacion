663594120b454bfaa30063f33e8ed377dcf5d0ed
profesorderedes <profesorderedes@gmail.com>
2022-01-25 16:02:31 +0100

    mar 25 ene 2022 16:02:31 CET

--- Tema4/src/Ejemplo02.java
+++ Tema4/src/Ejemplo02.java
@@ -1,4 +1,3 @@
-import java.util.Iterator;
 import java.util.Scanner;
 
 public class Ejemplo02 {
@@ -6,38 +5,60 @@ public class Ejemplo02 {
 	static float precios[] = new float[5];
 
 	public static void main(String[] args) {
-		
+
 		leerPrecios();
-		
+
 		ordenarPrecios();
-		
+
 		mostrarPrecios();
-		
+
 	}
-	
+
 	static void leerPrecios() {
-		
+
 		Scanner entrada = new Scanner(System.in);
-		
+
 		System.out.println("Escribe 5 precios:");
-		
+
 		for (int i = 0; i < precios.length; i++) {
-			
-			System.out.print("Precio " + (i+1) + ": ");
+
+			System.out.print("Precio " + (i + 1) + ": ");
 			precios[i] = entrada.nextFloat();
-			
+
 		}
-		
+
 	}
-	
+
 	static void ordenarPrecios() {
-		
-		
-		
+
+		int posMin;
+		float aux;
+
+		for (int i = 0; i < precios.length - 1; i++) {
+
+			posMin = i;
+
+			for (int j = i + 1; j < precios.length; j++) {
+				if (precios[j] < precios[posMin]) {
+					posMin = j;
+				}
+			}
+
+			aux = precios[i];
+			precios[i] = precios[posMin];
+			precios[posMin] = aux;
+
+		}
+
 	}
-	
+
 	static void mostrarPrecios() {
 		
+		System.out.println("Precios almacenados:");
+		for (int i = 0; i < precios.length; i++) {
+			System.out.print(precios[i] + "  ");
+		}
+
 	}
-	
+
 }
