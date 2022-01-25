766f2f9d420539f44b6846eb6a09c5da802b0ca0
profesorderedes <profesorderedes@gmail.com>
2022-01-25 16:56:24 +0100

    mar 25 ene 2022 16:56:24 CET

--- Tema4/src/Ejemplo06.java
+++ Tema4/src/Ejemplo06.java
@@ -1,13 +1,29 @@
+import java.util.Scanner;
 
 public class Ejemplo06 {
 	
 	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("¡Escribe dos números y te "
+				+ "los sumaré!");
+		System.out.println("Primer número: ");
+		float num1 = entrada.nextFloat();
+		
+		System.out.println("Segundo número: ");
+		float num2 = entrada.nextFloat();
+		
+		sumar(num1, num2);
 		
 	}
 	
 	// Método que recibe dos números decimales
 	// y muestre el valor de su suma.
 	static void sumar(float numero1, float numero2) {
+		
+		System.out.println("La suma vale " 
+		    + (numero1 + numero2));
 		
 	}
 
