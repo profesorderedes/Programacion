af25f7c8023380164f67911cfc7eb45f95d8a340
profesorderedes <profesorderedes@gmail.com>
2022-01-26 20:19:09 +0100

    mié 26 ene 2022 20:19:09 CET

--- Tema4/src/Ejemplo09.java
+++ Tema4/src/Ejemplo09.java
@@ -2,19 +2,58 @@
 public class Ejemplo09 {
 
 	public static void main(String[] args) {
+
+		System.out.println("\nEl triple de 5 es " + triple(5));
+
+		System.out.println("\nUn círculo de radio 5 tiene "
+				+ "un área de " + areaCirculo(5));
+		
+		System.out.println("\nUna esfera de radio 10 tiene "
+				+ "un volumen de " + volumenEsfera(10));
+		
+		System.out.println("\nUn triángulo de base 100 y "
+				+ "altura 20 tiene un área de " 
+				+ areaTriangulo(100,20));
+		
+		System.out.println("\nLa letra inicial de la palabra "
+				+ "aljibe es la " + inicial("aljibe"));
+		
+		System.out.println("\nSi un producto cuesta 100 euros, "
+				+ "le hacemos un descuento del 10% "
+				+ "y le aplicamos un IVA del 21%, su precio "
+				+ "final será " + precioVenta(100,10,21));
 		
 	}
 
 	// Método triple(num)
+	static int triple(int num) {
+		return 0;
+	}
 
 	// Método areaCirculo(r)
+	static float areaCirculo(float r) {
+		return 0;
+	}
 
 	// Método volumenEsfera(r)
-
+	static float volumenEsfera(float r) {
+		return 0;
+	}
+	
 	// Método areaTriangulo(base, altura)
+	static float areaTriangulo(float base, float altura) {
+		return 0;
+	}
 
 	// Método inicial(palabra)
+	static char inicial(String palabra) {
+		return 0;
+	}
 
 	// Método precioVenta(precio, descuento, iva)
+	static float precioVenta(float precio, float descuento, 
+			float iva) {
+		return 0;
+	}
 
 }
