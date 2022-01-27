3a655e8f6548c114d704def2735c5127c8a81399
profesorderedes <profesorderedes@gmail.com>
2022-01-27 16:15:30 +0100

    jue 27 ene 2022 16:15:30 CET

--- Tema4/src/Ejemplo10.java
+++ Tema4/src/Ejemplo10.java
@@ -1,18 +1,35 @@
+import java.util.Arrays;
 
 public class Ejemplo10 {
 
 	public static void main(String[] args) {
 		
 		int num = 6;
-		int lista[] = {9, 5, 8, 7, 3, 10};
+		int numeros[] = {9, 5, 8, 7, 3, 10};
 		
+		// La variable num se pasa por valor porque es un 
+		// tipo primitivo.
 		dobleNum(num);
-		dobleArray(lista);
+		
+		// La variable numeros se pasa por referencia porque
+		// es un objeto.
+		dobleArray(numeros);
+		
+		System.out.println("La variable num se ha pasado por "
+				+ "valor:");
+		System.out.println("num = " + num);
+		
+		System.out.println("La variable numeros se ha pasado "
+				+ "por referencia:");
+		System.out.println("numeros[0] = " + numeros[0]);
 		
 	}
 	
-	static void dobleNum(int num) {
-		System.out.println(num * 2);
+	static void dobleNum(int valor) {
+		
+		valor = valor * 2;
+		System.out.println(valor);
+		
 	}
 	
 	// Va a multiplicar cada valor del array lista por dos.
@@ -21,6 +38,8 @@ public class Ejemplo10 {
 		for (int i = 0; i < lista.length; i++) {
 			lista[i] = lista[i] * 2;
 		}
+		
+		System.out.println(Arrays.toString(lista));
 		
 	}
 	
