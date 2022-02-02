e0c9fcd432707c4a8e465b190467a07001d2945e
profesorderedes <profesorderedes@gmail.com>
2022-02-02 18:48:08 +0100

    mié 02 feb 2022 18:48:07 CET

--- Tema5/src/ejemplo04/Main.java
+++ Tema5/src/ejemplo04/Main.java
@@ -3,9 +3,19 @@ package ejemplo04;
 public class Main {
 
 	public static void main(String[] args) {
+
+		Punto p1 = new Punto();
+
+		p1.x = 2;
+		p1.y = 5;
+		p1.color = "Rojo";
+
+		System.out.println("x: " + p1.x);
+		System.out.println("y: " + p1.y);
+		System.out.println("Color: " + p1.color);
 		
-		p1
 		
+
 	}
-	
+
 }
