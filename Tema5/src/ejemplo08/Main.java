d0f25277d0c061e5d720a826dcf5a5c955d1833d
profesorderedes <profesorderedes@gmail.com>
2022-02-03 16:52:44 +0100

    jue 03 feb 2022 16:52:44 CET

--- Tema5/src/ejemplo08/Main.java
+++ Tema5/src/ejemplo08/Main.java
@@ -4,28 +4,10 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Punto p1 = new Punto();
-
-		p1.x = 2;
-		p1.y = 5;
-		p1.color = "Rojo";
-
-//		p1.mostrar();
+		Punto p1 = new Punto(10, 30, "Verde");
 		
-		Punto p2= new Punto();
-		Punto p3= new Punto();
+		p1.mostrar();
 		
-		p2.color = "amarillo";
-		p2.x = 2;
-		p2.y = 5;
-		
-		p3.color = "naranja";
-		p3.x = 3;
-		p3.y = 3;
-		
-		// Desplazamos el punto 2, 10 píxeles a la derecha.
-		p2.desplazar(10);
-		p2.mostrar();
 
 	}
 	
---
+++ Tema5/src/ejemplo09/Circulo.java
@@ -0,0 +1,38 @@
+package ejemplo09;
+
+public class Circulo {
+
+	// Atributos
+	int x;
+	int y;
+	int radio;
+	String color;
+	
+	// Constructor
+	
+
+	// Métodos
+	void mostrar() {
+
+		System.out.println("X: " + x);
+		System.out.println("Y: " + y);
+		System.out.println("Radio: " + radio);
+		System.out.println("Color: " + color);
+
+	}
+
+	double area() {
+		
+		return Math.PI * radio * radio;
+
+	}
+	
+	double perimetro() {
+		
+		return 2 * Math.PI * radio;
+		
+	}
+	
+	
+
+}
