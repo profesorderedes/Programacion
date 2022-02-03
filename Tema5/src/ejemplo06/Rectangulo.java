d5c231db4b05396f76b91de1edfeec350a86905b
profesorderedes <profesorderedes@gmail.com>
2022-02-03 15:32:47 +0100

    jue 03 feb 2022 15:32:47 CET

--- Tema5/src/ejemplo06/Rectangulo.java
+++ Tema5/src/ejemplo06/Rectangulo.java
@@ -35,14 +35,16 @@ public class Rectangulo {
 	
 	// Desplaza el rectángulo dx píxeles a la derecha y 
 	// dy píxeles hacia arriba.
-	
+	void desplazar(int dx, int dy) {
+		
+	}
 	
 	
 	
 	// Devuelve true si el punto (a,b) está dentro del 
 	// rectángulo, y false si no lo está.
 	boolean estaDentro(int a, int b) {
-		
+		return false;
 	}
 	
 
