5246dda65a06babbcc19a06bf5ae32dcaff75d15
profesorderedes <profesorderedes@gmail.com>
2022-02-03 16:07:40 +0100

    jue 03 feb 2022 16:07:39 CET

---
+++ Tema5/src/ejemplo07/Main.java
@@ -0,0 +1,30 @@
+package ejemplo07;
+
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+	
+		Persona persona1 = new Persona();
+		
+		// Leemos los datos de persona1 desde la terminal.
+		System.out.println("Nombre: ");
+		persona1.nombre = entrada.nextLine();
+		
+		System.out.println("Apellidos: ");
+		persona1.apellidos = entrada.nextLine();
+		
+		System.out.println("Edad: ");
+		persona1.edad = entrada.nextInt();
+		
+		// Los mostramos.
+		persona1.mostrar();
+		
+		entrada.close();
+		
+	}
+	
+}
---
+++ Tema5/src/ejemplo07/Persona.java
@@ -0,0 +1,22 @@
+package ejemplo07;
+
+public class Persona {
+
+	// Atributos
+	String nombre;
+	String apellidos;
+	int edad;
+
+	// Métodos
+	void mostrar() {
+
+		System.out.println("Datos de " + nombre);
+		System.out.println("===============================");
+
+		System.out.println("Nombre: " + nombre);
+		System.out.println("Apellidos: " + apellidos);
+		System.out.println("Edad: " + edad);
+
+	}
+
+}
