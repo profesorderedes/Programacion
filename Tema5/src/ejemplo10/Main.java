9f372b3a14e55bbc8a4a58d6f50c709590d43fa5
profesorderedes <profesorderedes@gmail.com>
2022-02-04 15:29:42 +0100

    vie 04 feb 2022 15:29:42 CET

--- Tema5/src/ejemplo10/Main.java
+++ Tema5/src/ejemplo10/Main.java
@@ -4,25 +4,15 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Rectangulo r1 = new Rectangulo();
-
-//		r1.x = 20;
-		r1.y = 50;
-		r1.base = 40;
-		r1.altura = 10;
-		r1.color = "Rojo";
-
+		Rectangulo r1 = new Rectangulo(10, 20, 50, 30, 
+				"Verde");
+		
 		r1.mostrar();
-
-		System.out.println("Área: " + r1.area());
-		System.out.println("Perímetro: " + r1.perimetro());
-
-//		r1.desplazar(30, 5);
-//		r1.mostrar();
-
-		System.out.println(r1.estaDentro(45, 55));
-		System.out.println(r1.estaDentro(100, 100));
-
+		
+		Rectangulo r2 = new Rectangulo();
+		r2.x = 50;
+		
+		
 	}
 
 }
