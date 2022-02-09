361f7b12cf60e1724eea01d34058dd37e9fcc992
profesorderedes <profesorderedes@gmail.com>
2022-02-09 20:22:58 +0100

    mié 09 feb 2022 20:22:58 CET

--- Tema5Proyecto1/src/geometria/Cilindro.java
+++ Tema5Proyecto1/src/geometria/Cilindro.java
@@ -5,15 +5,40 @@ public class Cilindro {
 	private int altura;
 	private int radio;
 	private final double PI = 3.141592;
-	
+
 	public Cilindro() {
-		
+
 	}
-	
+
 	public Cilindro(int altura, int radio) {
 		this.altura = altura;
 		this.radio = radio;
 	}
-	
-	
+
+	public double area() {
+		return 2 * PI * (radio * altura + Math.pow(radio, 2));
+	}
+
+	public double volumen() {
+		return PI * Math.pow(radio, 2) * altura;
+	}
+
+	public String toString() {
+
+		String resultado;
+
+		resultado = "altura = " + altura + "\nradio = " 
+				+ radio;
+
+		return resultado;
+
+	}
+
 }
+
+
+
+
+
+
+
--- Tema5Proyecto1/src/inicio/Inicio.java
+++ Tema5Proyecto1/src/inicio/Inicio.java
@@ -5,9 +5,13 @@ import geometria.Cilindro;
 public class Inicio {
 
 	public static void main(String[] args) {
+
+		Cilindro cilindro1 = new Cilindro(10, 20);
+
+//		System.out.println("Área: " + cilindro1.area());
+//		System.out.println("Volumen: " + cilindro1.volumen());
 		
-		Cilindro cilindro1 = new Cilindro();
-		
-		
+		System.out.println(cilindro1.toString());
+
 	}
 }
