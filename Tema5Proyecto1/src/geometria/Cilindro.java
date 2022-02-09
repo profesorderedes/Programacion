b82f8766079fc9227bfe2204dd889b1f8f502526
profesorderedes <profesorderedes@gmail.com>
2022-02-09 20:02:44 +0100

    mié 09 feb 2022 20:02:44 CET

---
+++ Tema5Proyecto1/src/geometria/Cilindro.java
@@ -0,0 +1,19 @@
+package geometria;
+
+public class Cilindro {
+
+	private int altura;
+	private int radio;
+	private final double PI = 3.141592;
+	
+	public Cilindro() {
+		
+	}
+	
+	public Cilindro(int altura, int radio) {
+		this.altura = altura;
+		this.radio = radio;
+	}
+	
+	
+}
---
+++ Tema5Proyecto1/src/inicio/Inicio.java
@@ -0,0 +1,13 @@
+package inicio;
+
+import geometria.Cilindro;
+
+public class Inicio {
+
+	public static void main(String[] args) {
+		
+		Cilindro cilindro1 = new Cilindro();
+		
+		
+	}
+}
