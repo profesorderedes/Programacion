a355862bf51bf873081f7ea22f4929b4247bb457
profesorderedes <profesorderedes@gmail.com>
2022-02-09 19:42:24 +0100

    mié 09 feb 2022 19:42:24 CET

--- Tema5/src/ejemplo14Persona/Main.java
+++ Tema5/src/ejemplo14Persona/Main.java
@@ -4,17 +4,41 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Persona persona1 = new Persona("Rubén","López Martín",
-				30);
-		
+		Persona persona1 = new Persona("Rubén", "López Martín", 30);
+
 		persona1.mostrar();
-		
-		Persona persona2 = new Persona();
-		persona2.mostrar();
-		
-		Persona persona3 = new Persona("Amparo", "López");
-		persona3.mostrar();
+
+		// Queremos modificar el nombre.
+		System.out.println("\nNombre original: " 
+				+ persona1.getNombre());
+
+		persona1.setNombre("Raúl");
+		System.out.println("\nNombre nuevo: " 
+				+ persona1.getNombre());
+
+		// Modificamos los apellidos.
+		System.out.println("\nApellidos originales: " 
+				+ persona1.getApellidos());
+		persona1.setApellidos("López Martínez");
+		System.out.println("\nApellidos nuevos: " 
+				+ persona1.getApellidos());
+
+		// Modificamos la edad.
+		System.out.println("\nEdad original: " 
+				+ persona1.getEdad());
+		persona1.setEdad(31);
+		System.out.println("\nEdad nueva: " 
+				+ persona1.getEdad());
 
 	}
 
 }
+
+
+
+
+
+
+
+
+
--- Tema5/src/ejemplo14Persona/Persona.java
+++ Tema5/src/ejemplo14Persona/Persona.java
@@ -3,25 +3,25 @@ package ejemplo14Persona;
 public class Persona {
 
 	// Atributos
-	String nombre;
-	String apellidos;
-	int edad;
-	
+	private String nombre;
+	private String apellidos;
+	private int edad;
+
 	// Constructores
-	Persona(){
-		
+	Persona() {
+
 	}
-	
-	Persona(String nombre, String apellidos){
+
+	Persona(String nombre, String apellidos) {
 		this(nombre, apellidos, 0);
 	}
-	
-	Persona(String nombre, String apellidos, int edad){
-		
+
+	Persona(String nombre, String apellidos, int edad) {
+
 		this.nombre = nombre;
 		this.apellidos = apellidos;
 		this.edad = edad;
-				
+
 	}
 
 	// Métodos
@@ -34,6 +34,31 @@ public class Persona {
 		System.out.println("Apellidos: " + apellidos);
 		System.out.println("Edad: " + edad);
 
+	}
+
+	// Métodos de acceso
+	String getNombre() {
+		return nombre;
+	}
+
+	void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	String getApellidos() {
+		return apellidos;
+	}
+
+	void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	int getEdad() {
+		return edad;
+	}
+
+	void setEdad(int edad) {
+		this.edad = edad;
 	}
 
 }
