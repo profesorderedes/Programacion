378c028507b7ae179557ce2fe328606cfea71292
profesorderedes <profesorderedes@gmail.com>
2022-02-10 16:51:39 +0100

    jue 10 feb 2022 16:51:39 CET

--- Tema5Proyecto2/src/conversion/Conversor.java
+++ Tema5Proyecto2/src/conversion/Conversor.java
@@ -2,7 +2,17 @@ package conversion;
 
 public class Conversor {
 
-	final static double KM_A_MILLAS = 0.621;
+	private static final double KM_A_MILLAS = 0.621;
+	private static final double MB_A_ATM = 0.000986;
+	private static final double CAL_A_JULIOS = 4.187;
+	
+	public static double millas(double km) {
+		return km * KM_A_MILLAS;	
+	}
+	
+	public static double km(double millas) {
+		return millas / KM_A_MILLAS;
+	}
 	
 	
 }
--- Tema5Proyecto2/src/inicio/Inicio.java
+++ Tema5Proyecto2/src/inicio/Inicio.java
@@ -1,7 +1,37 @@
 package inicio;
 
+import java.util.Scanner;
+
+import conversion.Conversor;
+
 public class Inicio {
 
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+
+		// Pregunta una cantidad en km y la pasa a millas.
+		System.out.println("Escribe una distancia en km:");
+		double distKm = entrada.nextDouble();
+
+		System.out.println(distKm + " km son " 
+				+ Conversor.millas(distKm) + " millas.");
+
+		// Pregunta una cantidad en millas y la pasa a km.
+		System.out.println("\nEscribe una distancia en millas:");
+		double distMillas = entrada.nextDouble();
+
+		System.out.println(distMillas + " millas son " 
+				+ Conversor.km(distMillas) + " km.");
+
 	
-	
+		
+		
+	}
+
 }
+
+
+
+
+
