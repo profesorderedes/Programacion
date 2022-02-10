b6e517bdfc30a483c6bd68194c16f56fa939b1d4
profesorderedes <profesorderedes@gmail.com>
2022-02-10 15:55:13 +0100

    jue 10 feb 2022 15:55:13 CET

---
+++ Tema5/src/ejemplo16Punto/Main.java
@@ -0,0 +1,24 @@
+package ejemplo16Punto;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Punto p1 = new Punto(10, 30, "Verde");
+		
+		p1.mostrar();
+		
+		Punto p2 = new Punto();
+		p2.mostrar();
+		
+		Punto p3 = new Punto("Rojo");
+		p3.mostrar();
+		
+		Punto p4 = new Punto(10, 15);
+		p4.mostrar();
+
+	}
+	
+	
+
+}
