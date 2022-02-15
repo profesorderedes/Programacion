97a743d6e946c101612f62cd97f96b7cc323d845
profesorderedes <profesorderedes@gmail.com>
2022-02-15 18:49:48 +0100

    mar 15 feb 2022 18:49:48 CET

--- Tema5proyecto3/src/biblioteca/Libro.java
+++ Tema5proyecto3/src/biblioteca/Libro.java
@@ -7,5 +7,17 @@ public class Libro {
 	private Autor autor;
 	private static int anyoActual;
 	
+	public Libro(String titulo) {
+		
+		this.titulo = titulo;
+		
+	}
 	
+	public Libro(String titulo, String isbn, Autor autor) {
+		
+		this.titulo = titulo;
+		this.isbn = isbn;
+		this.autor = autor;
+		
+	}
 }
