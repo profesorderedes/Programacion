08836e92af2313802acb3fcdd86d6bc6e916ce5d
profesorderedes <profesorderedes@gmail.com>
2022-02-18 17:09:03 +0100

    vie 18 feb 2022 17:09:03 CET

--- Tema5Proyecto8/src/instituto/Instituto.java
+++ Tema5Proyecto8/src/instituto/Instituto.java
@@ -3,90 +3,69 @@ package instituto;
 import auxiliar.Alumno;
 
 public class Instituto {
-	
+
 	private final int MAX_ALUMNOS = 100;
 	private Alumno[] alumnos = new Alumno[MAX_ALUMNOS];
 	private int numAlumnos = 0;
-	
+
 	public void addAlumno(Alumno alu) {
-		
+
 		for (int i = 0; i < alumnos.length; i++) {
-			
-			if(alumnos[i] == null) {
+
+			if (alumnos[i] == null) {
 				alumnos[i] = alu;
 				numAlumnos++;
 				break;
 			}
-			
+
 		}
-		
+
 	}
-	
+
 	public void delAlumno(int numExpediente) {
-		
-		for (int i = 0; i < alumnos.length; i++) {
-			
-			if(alumnos[i] != null 
-					&& alumnos[i].getNumExpediente() 
-						== numExpediente) {
-				alumnos[i] = null;
-				numAlumnos--;
-				break;
-			}
-			
+
+		int pos = buscaAlumno(numExpediente);
+
+		if (pos != -1) {
+			alumnos[pos] = null;
+			numAlumnos--;
 		}
-		
+
 	}
-	
+
 	public void muestraAlumnos() {
-		
+
 		System.out.println("\nListado de alumnos");
 		System.out.println("================================");
 		for (int i = 0; i < alumnos.length; i++) {
-			
-			if(alumnos[i] != null) {
-				System.out.println("- " 
-						+ alumnos[i].getApellidos() 
-						+ ", " + alumnos[i].getNombre()
-						+ " (" 
-						+ alumnos[i].getGrupo().getNivel()
-						+ ")");
+
+			if (alumnos[i] != null) {
+				System.out.println("- " + alumnos[i].getApellidos() + ", " + alumnos[i].getNombre() + " ("
+						+ alumnos[i].getGrupo().getNivel() + ")");
 			}
-			
+
 		}
-		
+
 	}
-	
+
 	public int numAlumnos() {
-		
+
 		return numAlumnos;
-		
+
 	}
-	
+
 	public int buscaAlumno(int numExpediente) {
-		
+
 		for (int i = 0; i < alumnos.length; i++) {
-			
-			if(alumnos[i] != null 
-					&& alumnos[i].getNumExpediente() 
-						== numExpediente) {
+
+			if (alumnos[i] != null && alumnos[i].getNumExpediente() == numExpediente) {
 				return i;
 			}
-			
+
 		}
-		
+
 		return -1;
-		
+
 	}
 
 }
-
-
-
-
-
-
-
-
-
-
