7e6b41a5c67b48693b094d37eb1a564d78cfbde0
profesorderedes <profesorderedes@gmail.com>
2022-03-03 20:25:43 +0100

    jue 03 mar 2022 20:25:43 CET

--- Tema6/src/ejemplo04Persona/Empleado.java
+++ Tema6/src/ejemplo04Persona/Empleado.java
@@ -1,15 +1,25 @@
 package ejemplo04Persona;
 
 public class Empleado extends Persona {
-	
+
 	private String numSegSocial;
-	
-	public Empleado(String dni, String nombre, 
-			String apellidos, String numSegSocial) {
-		
+
+	public Empleado(String dni, String nombre, String apellidos, String numSegSocial) {
+
 		super(dni, nombre, apellidos);
 		this.numSegSocial = numSegSocial;
-		
+
+	}
+
+	public void mostrar() {
+
+		System.out.println("Datos del cliente:");
+		System.out.println("DNI: " + getDni());
+		System.out.println("Apellidos: " + getApellidos());
+		System.out.println("Nombre: " + getNombre());
+		System.out.println("Número seg. social: " 
+				+ numSegSocial);
+
 	}
 
 }
