4a7e938b46c6dd46fe436e41aa12809f5c18028b
profesorderedes <profesorderedes@gmail.com>
2022-03-10 16:09:33 +0100

    jue 10 mar 2022 16:09:33 CET

--- Tema6/src/ejemplo06Persona/Cliente.java
+++ Tema6/src/ejemplo06Persona/Cliente.java
@@ -1,23 +1,28 @@
 package ejemplo06Persona;
 
-public class Cliente extends Persona{
-	
+public class Cliente extends Persona {
+
 	String visa;
-	
-	public Cliente(String dni, String nombre, 
-			String apellidos, String visa) {
-		
+
+	public Cliente(String dni, String nombre, String apellidos, String visa) {
+
 		super(dni, nombre, apellidos);
 		this.visa = visa;
-		
+
 	}
-	
+
 	public void mostrar() {
-		
+
 		System.out.println("\nDatos del cliente:");
 		super.mostrar();
 		System.out.println("Visa: " + visa);
-		
+
 	}
-	
+
+	public void mostrarVisa() {
+
+		System.out.println("La VISA del cliente es " + visa);
+
+	}
+
 }
--- Tema6/src/ejemplo06Persona/Main.java
+++ Tema6/src/ejemplo06Persona/Main.java
@@ -9,7 +9,8 @@ public class Main {
 		// de clases hijas.
 		Persona persona1;
 
-		persona1 = new Cliente("10299122J", "Carlos", "Pérez", "2910-1291-1215-5334");
+		persona1 = new Cliente("10299122J", "Carlos", "Pérez", 
+				"2910-1291-1215-5334");
 
 		// Al llamar a un método que existe en las dos clases
 		// (Persona y Cliente), se ejecutará el de la clase
@@ -17,10 +18,35 @@ public class Main {
 		persona1.mostrar();
 
 		// Lo mismo sucede con Empleado.
-		Persona persona2 = new Empleado("10011010Y", "María", "López", "2635eas");
+		Persona persona2 = new Empleado("10011010Y", "María", 
+				"López", "2635eas");
 
 		persona2.mostrar();
+		
+		// No podemos llamar a un método de Cliente que no 
+		// este en la clase padre (Persona).
+		// persona1.mostrarVisa();
+		
+		// A no ser que hagamos una conversión (cast)
+		((Cliente)persona1).mostrarVisa();
+		
+		// Cuando hacemos un cast, debemos estar seguros de que
+		// realmente el contenido de la variable es del tipo 
+		// al que estamos haciendo la conversión.
+
+		// La siguiente instrucción dará error, ya que persona2
+		// contiene un Empleado, no un Cliente.
+		((Cliente)persona2).mostrarVisa();
 
 	}
 
 }
+
+
+
+
+
+
+
+
+
