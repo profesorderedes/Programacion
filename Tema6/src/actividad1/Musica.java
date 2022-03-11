26f566d141345889aac530ae4b5b4271a51379a1
profesorderedes <profesorderedes@gmail.com>
2022-03-11 16:26:18 +0100

    vie 11 mar 2022 16:26:18 CET

--- Tema6/src/actividad1/Musica.java
+++ Tema6/src/actividad1/Musica.java
@@ -5,6 +5,15 @@ public class Musica extends Medio {
 	private String artista;
 	private String estilo;
 	
+	public Musica(String nombre, int duracion, 
+			String artista, String estilo) {
+		
+		super(nombre, duracion);
+		this.artista = artista;
+		this.estilo = estilo;
+		
+	}
+	
 	public void reproducir() {
 		System.out.println("Reproduciendo...");
 	}
--- Tema6/src/actividad1/Video.java
+++ Tema6/src/actividad1/Video.java
@@ -5,6 +5,10 @@ public class Video extends Medio {
 	private String director;
 	private String idioma;
 	
+	public Video() {
+		
+	}
+	
 	public void reproducir() {
 		System.out.println("Reproduciendo...");
 	}
