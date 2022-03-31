0b2ea748048b6b56ae9a26ddc946fb9ed9fff2e0
profesorderedes <profesorderedes@gmail.com>
2022-03-31 16:18:19 +0200

    jue 31 mar 2022 16:18:18 CEST

--- Tema7/src/Ejemplo16.java
+++ Tema7/src/Ejemplo16.java
@@ -16,19 +16,31 @@ public class Ejemplo16 extends JFrame {
 		
 		// Menú File
 		JMenu menuFile = new JMenu("File");
+		
 		JMenuItem itemNew = new JMenuItem("New");
 		JMenuItem itemOpenFile = new JMenuItem("Open file ...");
 		JMenuItem itemOpenProjects = new JMenuItem(
 				"Open Projects from File System");
+		
 		menuFile.add(itemNew);
 		menuFile.add(itemOpenFile);
 		menuFile.add(itemOpenProjects);
 		
 		// Menú Edit
+		JMenu menuEdit=new JMenu("Edit");
 		
+        JMenuItem itemUndo=new JMenuItem("Undo Typing");
+        JMenuItem itemRedo=new JMenuItem("Redo");
+        JMenuItem itemCut=new JMenuItem("Cut");
+        
+        menuEdit.add(itemUndo);
+        menuEdit.add(itemRedo);
+        menuEdit.addSeparator();
+        menuEdit.add(itemCut);
 		
 		// Añadimos los menús a la barra de menús.
 		barraMenu.add(menuFile);
+		barraMenu.add(menuEdit);
 		
 		setJMenuBar(barraMenu);
 		
