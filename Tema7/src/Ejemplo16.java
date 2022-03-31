9f0ac87bd27bf1b87a70dd9bda9707b574333da1
profesorderedes <profesorderedes@gmail.com>
2022-03-31 16:12:22 +0200

    jue 31 mar 2022 16:12:21 CEST

--- Tema7/src/Ejemplo16.java
+++ Tema7/src/Ejemplo16.java
@@ -11,6 +11,27 @@ public class Ejemplo16 extends JFrame {
 		
 		setLayout(new FlowLayout(FlowLayout.LEFT));
 		
+		// Barra de menú.
+		JMenuBar barraMenu = new JMenuBar();
+		
+		// Menú File
+		JMenu menuFile = new JMenu("File");
+		JMenuItem itemNew = new JMenuItem("New");
+		JMenuItem itemOpenFile = new JMenuItem("Open file ...");
+		JMenuItem itemOpenProjects = new JMenuItem(
+				"Open Projects from File System");
+		menuFile.add(itemNew);
+		menuFile.add(itemOpenFile);
+		menuFile.add(itemOpenProjects);
+		
+		// Menú Edit
+		
+		
+		// Añadimos los menús a la barra de menús.
+		barraMenu.add(menuFile);
+		
+		setJMenuBar(barraMenu);
+		
 		
 		
 		setVisible(true);
