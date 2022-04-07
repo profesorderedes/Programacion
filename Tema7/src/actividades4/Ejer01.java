067f4f0c4180acdfe1c301896a8156d5f5644834
profesorderedes <profesorderedes@gmail.com>
2022-04-07 15:32:14 +0200

    jue 07 abr 2022 15:32:14 CEST

--- Tema7/src/actividades4/Ejer01.java
+++ Tema7/src/actividades4/Ejer01.java
@@ -2,8 +2,20 @@ package actividades4;
 
 import java.awt.*;
 import javax.swing.*;
+import java.awt.event.*;
 
-public class Ejer01 extends JFrame {
+public class Ejer01 extends JFrame implements ActionListener {
+
+	private JButton btnBlackRain;
+	private JButton btnWinslow;
+	private JButton btnAsalto;
+	private JButton btnBladeRunner;
+	private ImageIcon imgBladeRunner;
+	private ImageIcon imgWinslow;
+	private ImageIcon imgAsalto;
+	private ImageIcon imgBlackRain;
+
+	private JLabel lblimagen;
 
 	public Ejer01() {
 		super("Fotogramas");
@@ -16,31 +28,38 @@ public class Ejer01 extends JFrame {
 
 		JPanel panelBotones = new JPanel();
 
-		JButton btnBlackRain = new JButton("Black Rain");
-		JButton btnWinslow = new JButton("El caso Winslow");
-		JButton btnAsalto = new JButton("Asalto al distrito 13");
-		JButton btnBladeRunner = new JButton("Blade Runner");
+		btnBlackRain = new JButton("Black Rain");
+		btnBlackRain.addActionListener(this);
+
+		btnWinslow = new JButton("El caso Winslow");
+		btnWinslow.addActionListener(this);
+
+		btnAsalto = new JButton("Asalto al distrito 13");
+		btnAsalto.addActionListener(this);
+
+		btnBladeRunner = new JButton("Blade Runner");
+		btnBladeRunner.addActionListener(this);
 
 		panelBotones.add(btnBlackRain);
 		panelBotones.add(btnWinslow);
 		panelBotones.add(btnAsalto);
 		panelBotones.add(btnBladeRunner);
-		
-		
 
 		// FOTOGRAFÍA ABAJO
 
-		ImageIcon imgBladeRunner = new ImageIcon("iconos/bladerunner.jpg");
-		JLabel lblimagen = new JLabel(imgBladeRunner);
+		imgBladeRunner = new ImageIcon("iconos/bladerunner.jpg");
+		imgWinslow = new ImageIcon("iconos/the-winslow-boy.jpg");
+		imgAsalto = new ImageIcon("iconos/distrito13.jpg");
+		imgBlackRain = new ImageIcon("iconos/blackrain.jpg");
+
+		lblimagen = new JLabel(imgBladeRunner);
 
 		// VENTANA
 
 		add(panelBotones);
 		add(lblimagen);
 
-		
 		setVisible(true);
-		
 		btnBladeRunner.requestFocus();
 
 	}
@@ -48,6 +67,23 @@ public class Ejer01 extends JFrame {
 	public static void main(String[] args) {
 
 		new Ejer01();
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JButton boton = (JButton) e.getSource();
+
+		if (boton == btnBladeRunner) {
+			lblimagen.setIcon(imgBladeRunner);
+		} else if (boton == btnWinslow) {
+			lblimagen.setIcon(imgWinslow);
+
+		} else if (boton == btnAsalto) {
+			lblimagen.setIcon(imgAsalto);
+		} else {
+			lblimagen.setIcon(imgBlackRain);
+		}
 
 	}
 
