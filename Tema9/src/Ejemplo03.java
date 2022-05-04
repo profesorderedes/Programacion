4af06e5fda09cf7ce73b8928cb22511bb832fee5
profesorderedes <profesorderedes@gmail.com>
2022-05-04 20:22:18 +0200

    mié 04 may 2022 20:22:18 CEST

---
+++ Tema9/src/Ejemplo03.java
@@ -0,0 +1,38 @@
+import java.io.BufferedOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Iterator;
+
+public class Ejemplo03 {
+
+	public static void main(String[] args) {
+		
+		try {
+			// Abrimos el archivo para escritura.
+			FileOutputStream archivo = 
+					new FileOutputStream("ficheros/numeros.txt");
+			
+			// Creamos un buffer para acceder al archivo.
+			BufferedOutputStream buffer = 
+					new BufferedOutputStream(archivo);
+			
+			// Escribimos en el archivo los números del 50 al 100.
+			for (int i = 50; i <= 100; i++) {
+				buffer.write(i);
+			}
+			
+			// Cerramos el buffer.
+			buffer.close();
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+	
+}
---
+++ Tema9/src/Ejemplo04.java
@@ -0,0 +1,43 @@
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Ejemplo04 {
+	
+	public static void main(String[] args) {
+		
+		
+		try {
+			// Abrimos el archivo para lectura.
+			FileInputStream archivo = 
+					new FileInputStream("ficheros/numeros.txt");
+			
+			// Creamos un buffer de lectura.
+			BufferedInputStream buffer = 
+					new BufferedInputStream(archivo);
+			
+			// Leemos el primer byte del archivo.
+			int byteLeido = buffer.read();
+			
+			// Vamos leyendo consecutivamente todos los bytes,
+			// hasta llegar al final del archivo.
+			while(byteLeido != -1) {
+				
+				System.out.println(byteLeido);
+				byteLeido = buffer.read();
+				
+			}
+			
+			// Cerramos el archivo.
+			buffer.close();
+						
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
