b79c4e7fe9980b8355626964ca10a6115311d857
profesorderedes <profesorderedes@gmail.com>
2022-05-05 16:50:17 +0200

    jue 05 may 2022 16:50:17 CEST

--- Tema9/src/Ejemplo06.java
+++ Tema9/src/Ejemplo06.java
@@ -3,34 +3,31 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class Ejemplo06 {
-	
+
 	public static void main(String[] args) {
-		
+
 		// Información que queremos guardar.
 		String nombre = "charlie";
 		int puntos = 10;
-		
-		
+
 		try {
 			// Abrimos el archivo para escritura.
-			FileWriter archivo = new FileWriter(
-					"ficheros/puntuaciones.txt");
+			FileWriter archivo = new FileWriter("ficheros/puntuaciones.txt", true);
 			BufferedWriter buffer = new BufferedWriter(archivo);
-			
+
 			// Guardamos el nombre del jugador y su puntuación.
 			buffer.write(nombre + "\n");
-			buffer.write(puntos+"");
-			
+			buffer.write(puntos + "\n");
+
 			// Cerramos el archivo.
 			buffer.close();
-			
+
 			System.out.println("Se ha guardado la puntuación.");
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		
+
 	}
 
 }
