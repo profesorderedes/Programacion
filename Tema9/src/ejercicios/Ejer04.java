2944e44796244e9f95303ed04cb405fdc2706cc8
profesorderedes <profesorderedes@gmail.com>
2022-05-10 19:37:53 +0200

    mar 10 may 2022 19:37:53 CEST

--- Tema9/src/ejercicios/Ejer04.java
+++ Tema9/src/ejercicios/Ejer04.java
@@ -10,6 +10,8 @@ public class Ejer04 {
 	public static void main(String[] args) {
 
 		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println();
 
 		System.out.print("Título de la página: ");
 		String titulo = entrada.nextLine();
