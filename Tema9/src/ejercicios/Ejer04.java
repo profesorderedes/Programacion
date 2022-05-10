c2d01807c1e84e8e926763df423b4f7d71e8a404
profesorderedes <profesorderedes@gmail.com>
2022-05-10 19:37:12 +0200

    mar 10 may 2022 19:37:12 CEST

--- Tema9/src/ejercicios/Ejer04.java
+++ Tema9/src/ejercicios/Ejer04.java
@@ -6,48 +6,39 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Ejer04 {
-	
+
 	public static void main(String[] args) {
-		
+
 		Scanner entrada = new Scanner(System.in);
-		
-		// Pedimos los datos de la página.
+
 		System.out.print("Título de la página: ");
 		String titulo = entrada.nextLine();
-		
-		
+
+		System.out.print("Encabezado de la página: ");
+		String encabezado = entrada.nextLine();
+
+		System.out.print("Contenido de la página: ");
+		String contenido = entrada.nextLine();
+
 		try {
-			// Abrimos el archivo para escritura.
-			BufferedWriter buffer = new BufferedWriter(
-					new FileWriter("ficheros/index.html"));
-			
+			BufferedWriter buffer = 
+					new BufferedWriter(
+							new FileWriter("ficheros/index.html"));
+
 			buffer.write("<!DOCTYPE html>\n");
-			buffer.write("<html>\n");
-			buffer.write("<head>\n");
-			buffer.write("<title>" + titulo + "</title>\n");
-			
+			buffer.write("<html>\n<head>\n" + "<title>" + titulo + "</title>\n</head>\n");
+			buffer.write("<body>\n<h1>" + encabezado + "</h1>\n");
+			buffer.write("<p>" + contenido + "</p>\n");
+			buffer.write("</body>\n</html>");
+
 			buffer.close();
-			
-			
+
+			System.out.println("Se ha creado la página en tu carpeta personal. De nada");
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
 	}
 
 }
