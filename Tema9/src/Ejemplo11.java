3d800f3c99a44a3b2ff5c488d1f3c8329af3d619
profesorderedes <profesorderedes@gmail.com>
2022-05-13 16:24:03 +0200

    vie 13 may 2022 16:24:03 CEST

---
+++ Tema9/src/Ejemplo11.java
@@ -0,0 +1,25 @@
+
+/*
+ * Escribe un programa que cree la siguiente
+ * estructura de carpetas en tu carpeta personal:
+ * ejemplo11
+ * 	 info
+ * Dentro de la carpeta info, aparecerá un archivo 
+ * de texto "version.txt" con la siguiente información:
+
+	Ejemplo11
+	Autor: tu nombre
+	Versión: 1.0
+
+	Finalmente, el programa renombrará este archivo como 
+	"acercade.txt".
+ */
+public class Ejemplo11 {
+	
+	public static void main(String[] args) {
+		
+		
+		
+	}
+
+}
