7b2985b3217b096441d4ff784578ea85b02b7c57
Carlos <profesorderedes@gmail.com>
2022-05-14 13:00:43 +0200

    Corregidas algunas discrepancias con la versiones

--- Biblioteca/src/v0_8/App.java
+++ Biblioteca/src/v0_8/App.java
@@ -4,42 +4,65 @@ import java.util.Scanner;
 
 public class App {
 
-	public static void main(String[] args) {
+	private static Scanner entrada = new Scanner(System.in);
 
-		Scanner entrada = new Scanner(System.in);
+	public static void main(String[] args) {
 
 		mostrarPresentacion();
 
 		while (true) {
-			System.out.println(
-					"\n\nMENÚ PRINCIPAL\n======================================================================================================================");
 
-			System.out.println(
-					"1) Introducir libro\n2) Editar libro\n3) Borrar libro\n4) Ver todos los libros\n5) Salir");
-
-			System.out.print("\n¿Opción? ");
-			int opcion = entrada.nextInt();
+			int opcion = menuPrincipal();
 
 			switch (opcion) {
-
-			case 1, 2, 3, 4, 5:
-				System.out.println("\nHas elegido la opción " + opcion);
+			case 1:
+				System.out.println("\nHas elegido la opción 1");
+				break;
+			case 2:
+				System.out.println("\nHas elegido la opción 2");
+				break;
+			case 3:
+				System.out.println("\nHas elegido la opción 3");
+				break;
+			case 4:
+				System.out.println("\nHas elegido la opción 4");
+				break;
+			case 5:
+				System.out.println("\nHas elegido la opción 5");
+				System.exit(0);
+				break;
 			}
+
 		}
 
 	}
 
-	private static void mostrarPresentacion() {
-		
-		System.out.println("#################################################################################");
-		System.out.println("#\t\t\tBIBLIOTECA DEL IES SON FERRER\t\t\t\t#");
-		System.out.println("#\t\t\t   BASE DE DATOS DE LIBROS     \t\t\t\t#");
-		System.out.println("#################################################################################");
-		
+	public static void mostrarPresentacion() {
+
+		System.out.println("###################################################################################");
+		System.out.println("#                          BIBLIOTECA DEL IES SON FERRER                          #");
+		System.out.println("#                             BASE DE DATOS DE LIBROS                             #");
+		System.out.println("###################################################################################");
+
 	}
-	
-//	private static int menuPrincipal() {
-//		
-//	}
+
+	public static int menuPrincipal() {
+
+		System.out.println("\n\nMENÚ PRINCIPAL");
+		System.out.println(
+				"============================================================================================================");
+
+		System.out.println("1) Introducir libro");
+		System.out.println("2) Editar libro");
+		System.out.println("3) Borrar libro");
+		System.out.println("4) Ver todos los libros");
+		System.out.println("5) Salir");
+		System.out.print("\nOpción? ");
+
+		int opcion = entrada.nextInt();
+
+		return opcion;
+
+	}
 
 }
