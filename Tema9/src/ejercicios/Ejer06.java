962198f2c81c6125d7092879a67418bac7bf039a
profesorderedes <profesorderedes@gmail.com>
2022-05-18 20:36:42 +0200

    mié 18 may 2022 20:36:42 CEST

--- Tema9/src/ejercicios/Ejer06.java
+++ Tema9/src/ejercicios/Ejer06.java
@@ -1,3 +1,4 @@
+package ejercicios;
 
 import java.io.File;
 import javax.swing.JFileChooser;
@@ -11,12 +12,12 @@ import javax.swing.JFileChooser;
  *
  * @author csogorb
  */
-public class EjemploJFileChooser extends javax.swing.JFrame {
+public class Ejer06 extends javax.swing.JFrame {
 
     /**
      * Creates new form EjemploJFileChooser
      */
-    public EjemploJFileChooser() {
+    public Ejer06() {
         initComponents();
     }
 
@@ -140,20 +141,20 @@ public class EjemploJFileChooser extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(EjemploJFileChooser.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Ejer06.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(EjemploJFileChooser.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Ejer06.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(EjemploJFileChooser.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Ejer06.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(EjemploJFileChooser.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Ejer06.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new EjemploJFileChooser().setVisible(true);
+                new Ejer06().setVisible(true);
             }
         });
     }
