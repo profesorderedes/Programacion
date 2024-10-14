8af47ce48b042e0f1c0f20b0f950be5f60f0afb5
carlos <profesorderedes@gmail.com>
2024-10-14 16:01:57 +0200

    lun 14 oct 2024 16:01:57 CEST

--- Tema2/src/ejercicios/Ejer17.java
+++ Tema2/src/ejercicios/Ejer17.java
@@ -3,22 +3,58 @@ package ejercicios;
 import java.util.Scanner;
 
 public class Ejer17 {
-	
+
 	public static void main(String[] args) {
-		
+
 		Scanner entrada = new Scanner(System.in);
-		
+
 		System.out.println("Dime el número de mes y te diré ćomo se llama:");
 		int numMes = entrada.nextInt();
-		
+
 		String mes;
-		switch(numMes) {
+		switch (numMes) {
 		case 1:
 			mes = "Enero";
 			break;
 		case 2:
-			
+			mes = "Febrero";
+			break;
+		case 3:
+			mes = "Marzo";
+			break;
+		case 4:
+			mes = "Abril";
+			break;
+		case 5:
+			mes = "Mayo";
+			break;
+		case 6:
+			mes = "Junio";
+			break;
+		case 7:
+			mes = "Julio";
+			break;
+		case 8:
+			mes = "Agosto";
+			break;
+		case 9:
+			mes = "Septiembre";
+			break;
+		case 10:
+			mes = "Octubre";
+			break;
+		case 11:
+			mes = "Noviembre";
+			break;
+		case 12:
+			mes = "Diciembre";
+			break;
+		default:
+			mes = "Error";
 		}
+
+		System.out.println("El mes es " + mes + ".");
+
 	}
 
 }
