e603e2196c0d3e645be384f99f1de9bdab1e9e42
carlos <profesorderedes@gmail.com>
2024-10-14 15:45:37 +0200

    lun 14 oct 2024 15:45:37 CEST

---
+++ Tema2/src/Ejemplo06.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Ejemplo06 {
+
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.println("Color del semáforo: ");
+		String color = entrada.nextLine();
+
+		// Pasamos color a minúsculas, para que la salida no dependa
+		// de cómo lo escriba el usuario.
+		color = color.toLowerCase();
+		
+		// En función del valor de "color", se indicará una cosa 
+		// u otra.
+		switch (color) {
+		case "verde":
+			System.out.println("Continúa circulando.");
+			break;
+		case "naranja":
+			System.out.println("Detente si te da tiempo.");
+			break;
+		case "rojo":
+			System.out.println("Párate.");
+			break;
+		default:
+			System.out.println("Tienes que introducir un color válido.");
+		}
+
+	}
+
+}
---
+++ Tema2/src/Ejemplo07.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Ejemplo07 {
+	
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		// En un teatro tienen entradas de tres tipos: 1, 2 o 3.
+		// Según el tipo, las entradas tienen distinto valor.
+		
+		System.out.println("¿De qué tipo quieres la entrada (1/2/3)?");
+		int tipo = entrada.nextInt();
+		
+		switch()
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
