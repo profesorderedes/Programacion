387bbc15bcec93010397640edc7fd50203869300
carlos <profesorderedes@gmail.com>
2024-10-14 15:52:21 +0200

    lun 14 oct 2024 15:52:21 CEST

---
+++ Tema2/src/ejercicios/Ejer17.java
@@ -0,0 +1,24 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class Ejer17 {
+	
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Dime el número de mes y te diré ćomo se llama:");
+		int numMes = entrada.nextInt();
+		
+		String mes;
+		switch(numMes) {
+		case 1:
+			mes = "Enero";
+			break;
+		case 2:
+			
+		}
+	}
+
+}
