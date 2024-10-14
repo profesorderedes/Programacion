88d6c60a15e5fe55d26f5e2cdcadb41e0b0f913b
carlos <profesorderedes@gmail.com>
2024-10-14 16:57:31 +0200

    lun 14 oct 2024 16:57:31 CEST

---
+++ Tema2/src/ejercicios/Ejer19a.java
@@ -0,0 +1,22 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+// Ejercicio previo al 19 de la hoja 1 de condicionales.
+
+// Se pide al usuario un número de año y el programa nos dice si 
+// es bisiesto o no.
+
+public class Ejer19a {
+
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Dime un número de año:");
+		int anyo = entrada.nextInt();
+		
+		
+
+	}
+}
---
+++ Tema2/src/ejercicios/Ejer19b.java
@@ -0,0 +1,6 @@
+package ejercicios;
+
+// Ejercicio 19 de la hoja 1 de condicionales.
+public class Ejer19b {
+
+}
