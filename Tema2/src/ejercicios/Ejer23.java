9e29d189b2abfd13af1cc5b1e43b6ef321a593ac
carlos <profesorderedes@gmail.com>
2024-10-16 18:32:58 +0200

    mié 16 oct 2024 18:32:58 CEST

---
+++ Tema2/src/ejercicios/Ejer23.java
@@ -0,0 +1,38 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class Ejer23 {
+
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.println("Escribe un número entero, entre el 1 y el 10");
+
+		if (entrada.hasNextInt()) {
+
+			int numEnt = entrada.nextInt();
+
+			if(numEnt == 3 ) {
+				
+				System.out.println("Has acertado.");
+				
+			} else if (numEnt >= 1 && numEnt <= 10) {
+				
+				System.out.println(numEnt + ", es un número entero.");
+				
+			} else {
+				
+				System.out.println("Había que escribir un número del 1 al 10.");
+			}
+
+		} else {
+
+			System.out.println("Error");
+
+		}
+
+	}
+
+}
