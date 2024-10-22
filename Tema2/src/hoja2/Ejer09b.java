8296f9b99a33628474be8d0e507ab2e9fcb1d89d
carlos <profesorderedes@gmail.com>
2024-10-22 18:35:08 +0200

    mar 22 oct 2024 18:35:08 CEST

---
+++ Tema2/src/hoja2/Ejer09b.java
@@ -0,0 +1,30 @@
+package hoja2;
+
+import java.util.Scanner;
+
+public class Ejer09b {
+
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.println("Inserte el número a dividir");
+		int dividendo = entrada.nextInt();
+
+		System.out.println("Inserte el dividendo");
+		int divisor = entrada.nextInt();
+		
+		boolean seguirRestando = true;
+
+		while (seguirRestando) {
+
+			dividendo -= divisor;
+			System.out.println(dividendo + " - " + divisor + " = " + dividendo);
+
+		}
+
+		System.out.println("El resto será " + dividendo);
+
+	}
+
+}
