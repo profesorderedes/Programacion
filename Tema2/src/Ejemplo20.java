f8922f4e0464b5f9cb9696f350ff29c97ba6cbf8
carlos <profesorderedes@gmail.com>
2024-10-28 16:58:09 +0100

    lun 28 oct 2024 16:58:09 CET

--- Tema2/src/Ejemplo20.java
+++ Tema2/src/Ejemplo20.java
@@ -1,13 +1,26 @@
+import java.util.Scanner;
 
 public class Ejemplo20 {
 
 	public static void main(String[] args) {
-		
+
 		// Programa que pide diez notas, pero si una nota leída es
 		// -1, se entiende que no quedan notas para introducir.
-		
-		
-		
+
+		Scanner entrada = new Scanner(System.in);
+
+		for (int i = 1; i <= 10; i++) {
+
+			System.out.print("Nota: ");
+			int nota = entrada.nextInt();
+
+			if (nota == -1) {
+				break;
+			}
+		}
+
+		System.out.println("Se ha terminado de leer notas.");
+
 	}
-	
+
 }
