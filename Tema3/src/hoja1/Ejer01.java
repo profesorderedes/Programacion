c4e24efcfc9762aa55c86cd0afc3f3f135981623
carlos <profesorderedes@gmail.com>
2024-11-08 15:25:23 +0100

    vie 08 nov 2024 15:25:23 CET

---
+++ Tema3/src/hoja1/Ejer01.java
@@ -0,0 +1,17 @@
+package hoja1;
+
+public class Ejer01 {
+
+	public static void main(String[] args) {
+
+		String[] estaciones = { "Primavera", "Verano", "Otoño", "Invierno" };
+
+		for (int i = 0; i < estaciones.length; i++) {
+
+			System.out.println(estaciones[i]);
+
+		}
+
+	}
+
+}
---
+++ Tema3/src/hoja1/Ejer02.java
@@ -0,0 +1,21 @@
+package hoja1;
+
+public class Ejer02 {
+
+	public static void main(String[] args) {
+
+		String[] meses = { "enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre",
+				"octubre", "noviembre", "diciembre" };
+
+		int i = 0;
+
+		while (i < meses.length) {
+
+			System.out.println(meses[i]);
+			i++;
+
+		}
+
+	}
+
+}
