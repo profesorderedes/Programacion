a7bf5cbcbbcc1b5df07325b884c6102ba3f2d48a
carlos <profesorderedes@gmail.com>
2024-11-11 16:18:03 +0100

    lun 11 nov 2024 16:18:02 CET

--- Tema3/src/Ejemplo02.java
+++ Tema3/src/Ejemplo02.java
@@ -10,37 +10,23 @@ public class Ejemplo02 {
 		String[] nombres = { "Pepe", "María", "Juana" };
 
 		// Recorremos los arrays con bucles.
-		
-		for(int i = 0; i<=9; i++) {
+
+		for (int i = 0; i <= 9; i++) {
 			System.out.println(notas[i]);
 		}
-		
+
 		// Tamaño de un array (cantidad de elementos que tiene).
-		
+
 		System.out.println("\nTamaño del array notas: " + notas.length);
 		System.out.println("\nTamaño del array nombres: " + nombres.length);
-		
+
 		// Recorremos los arrays usando la propiedad length.
-		
+
 		System.out.println();
-		for(int i = 0; i < notas.length; i++) {
+		for (int i = 0; i < notas.length; i++) {
 			System.out.println(notas[i]);
 		}
-		
+
 	}
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- Tema3/src/hoja1/Ejer03.java
+++ Tema3/src/hoja1/Ejer03.java
@@ -2,14 +2,14 @@ package hoja1;
 
 public class Ejer03 {
 	public static void main(String[] args) {
-		
+
 		double[] densidades = { 5.427, 5.243, 5.514, 3.933, 1.326, 0.687, 1.270, 1.638, 1.860 };
-		
+
 		String[] nombres = { "Mercurio", "Venus", "Tierra", "Marte", "Jupiter", "Saturno", "Urano", "Neptuno",
 				"Pluton" };
 
 		for (int i = 0; i < nombres.length; i++) {
-			
+
 			System.out.println(nombres[i] + ": " + densidades[i] + "g/cm3");
 
 		}
