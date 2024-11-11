b69f1d7e3337808a3f5ad270d22e4d6b713a77f1
carlos <profesorderedes@gmail.com>
2024-11-11 19:26:18 +0100

    lun 11 nov 2024 19:26:18 CET

--- Tema3/src/hoja1/Ejer14a.java
+++ Tema3/src/hoja1/Ejer14a.java
@@ -1,11 +1,28 @@
 package hoja1;
 
+import java.util.Scanner;
+
 // Programa que te pregunta un número entero positivo y te dice 
 // si es primo o compuesto.
 public class Ejer14a {
 
 	public static void main(String[] args) {
 
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.println("Dime un número y te digo si es primo:");
+		int num = entrada.nextInt();
+		
+		// Probamos a dividir el número por todos los que están entre 
+		// 1 y él (sin incluir los extremos).
+		
+		
+
 	}
 
 }
+
+
+
+
+
