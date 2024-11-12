80d4b26b8e571e1d96368d6d9ca095e4f375d861
carlos <profesorderedes@gmail.com>
2024-11-12 17:20:34 +0100

    mar 12 nov 2024 17:20:34 CET

--- Tema3/src/hoja1/Ejer11b.java
+++ Tema3/src/hoja1/Ejer11b.java
@@ -3,8 +3,11 @@ package hoja1;
 import java.util.Scanner;
 
 // Actividad: modifica este ejercicio para que no permita escribir nada
+
 // que no sea una de las cinco órdenes. Si el usuario se equivoca, la
-// orden no se guardará y se le volverá a preguntar, las veces que 
+
+// orden no se guardará y se le volverá a preguntar, las veces que
+
 // sea necesario.
 
 public class Ejer11b {
@@ -15,15 +18,32 @@ public class Ejer11b {
 
 		String[] ordenes = new String[1000];
 
+		//String[] ordenes2 = { "izquierda", "derecha", "arriba", "abajo", "matar" };
+
 		int i = 0;
+		String orden;
 
 		while (i < ordenes.length) {
 
 			System.out.println("Orden número " + (i + 1) + ": ");
-			ordenes[i] = entrada.nextLine();
+			orden = entrada.nextLine();
 
-			if (ordenes[i].equals("ejecutar")) {
+			if (orden.equals("izquierda") || orden.equals("derecha") 
+					|| orden.equals("arriba") || orden.equals("abajo") 
+					|| orden.equals("matar")) {
+
+				ordenes[i] = orden;
+
+			} else if (orden.equals("ejecutar")) {
+
+				ordenes[i] = orden;
 				break;
+
+			} else {
+
+				System.out.println("Orden incorrecta");
+				continue;
+
 			}
 
 			i++;
