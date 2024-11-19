dbd6e5f1d859037c353adfeff1face2b5dc0eee5
carlos <profesorderedes@gmail.com>
2024-11-19 18:39:46 +0100

    mar 19 nov 2024 18:39:46 CET

---
+++ Tema3/src/hoja1/Ejer14d.java
@@ -0,0 +1,10 @@
+package hoja1;
+
+// Programa que te muestra en la terminal los 100 primeros números primos y además los guarda en un array.
+public class Ejer14d {
+
+	public static void main(String[] args) {
+
+	}
+
+}
