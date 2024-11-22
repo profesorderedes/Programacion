97e70b156df06ea973ffd746cf8d1870cdb0b94f
carlos <profesorderedes@gmail.com>
2024-11-22 16:12:06 +0100

    vie 22 nov 2024 16:12:05 CET

---
+++ Tema3/src/Ejemplo05.java
@@ -0,0 +1,26 @@
+
+// Algoritmo para obtener el mínimo de un array y la posición donde se encuentra.
+public class Ejemplo05 {
+
+	public static void main(String[] args) {
+
+		double[] precios = { 10.20, 3.15, 25, 1.55, 8 };
+
+		double min = precios[0];
+		int posMin = 0;
+
+		for (int i = 1; i < precios.length; i++) {
+
+			if (precios[i] < min) {
+				min = precios[i];
+				posMin = i;
+			}
+
+		}
+
+		System.out.println("El precio mínimo en el array es " + min + " euros.");
+		System.out.println("Y se encuentra en la posición " + posMin + ".");
+
+	}
+
+}
