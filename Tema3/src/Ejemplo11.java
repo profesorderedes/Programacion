06c81fb3cff39ac2174051e6e502992d4483e3d8
carlos <profesorderedes@gmail.com>
2024-11-28 17:52:41 +0100

    jue 28 nov 2024 17:52:41 CET

---
+++ Tema3/src/Ejemplo11.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+
+public class Ejemplo11 {
+
+	public static void main(String[] args) {
+
+		// String[][] salarios = new String[4][3];
+
+		String[][] salarios = { { "Sánchez", "María", "1800" }, { "Rodríguez", "Juan", "1700" },
+				{ "Pérez", "Nuria", "1750" }, { "López", "Luis", "1800" } };
+
+		System.out.println("Salarios de la empresa");
+		System.out.println("===================================");
+		for (int fila = 0; fila < salarios.length; fila++) {
+
+			// En la fila en la que estemos (fila) vamos recorriendo
+			// todas las columnas.
+			for (int col = 0; col < salarios[0].length; col++) {
+				System.out.print(salarios[fila][col] + "\t");
+			}
+			System.out.println();
+
+		}
+
+		// En Java, un array bidimensional es un array unidimensional
+		// donde cada posición contiene otro array (fila).
+		System.out.println("\nLa fila 0 de salarios es " + Arrays.toString(salarios[0]));
+		System.out.println("\nLa fila 1 de salarios es " + Arrays.toString(salarios[1]));
+
+		// El tamaño (.length) de un array bidimensional es el
+		// número de filas.
+		System.out.println("\nNúmero de filas: " + salarios.length);
+
+		// El tamaño (.length) de una fila es el número de columnas.
+		System.out.println("\nNúmero de columnas: " + salarios[0].length);
+
+	}
+}
---
+++ Tema3/src/Ejemplo12.java
@@ -0,0 +1,27 @@
+
+public class Ejemplo12 {
+	public static void main(String[] args) {
+		
+		// Tabla con temperaturas y presiones.
+		double[][] clima = {
+				{15, 1020},
+				{14, 1020},
+				{12, 1022},
+				{11.5, 1023},
+				{11, 1025}
+		};
+		
+		System.out.println("Datos climáticos");
+		System.out.println("===============================");
+		
+		for(int fila = 0; fila < clima.length; fila++) {
+			
+			for(int col = 0; col < clima[0].length; col++) {
+				System.out.print(clima[fila][col] + "\t");
+			}
+			System.out.println();
+			
+		}
+		
+	}
+}
