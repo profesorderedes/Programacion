e17e1c8ce3548d642ad79a69459a20fc6a85e0d3
carlos <profesorderedes@gmail.com>
2024-12-05 18:02:09 +0100

    jue 05 dic 2024 18:02:08 CET

--- Tema3/src/ejerciciosCadenas/Ejer07.java
+++ Tema3/src/ejerciciosCadenas/Ejer07.java
@@ -3,19 +3,37 @@ package ejerciciosCadenas;
 import java.util.Scanner;
 
 public class Ejer07 {
-	
+
 	public static void main(String[] args) {
-		
+
 		Scanner entrada = new Scanner(System.in);
-		String[] palabrotas = {"botarate", "bribón" };
-		
+		String[] palabrotas = { "botarate", "bribón", "mastuerzo", "mentecato", "rufián" };
+
 		System.out.println("### CENSOR ###");
 		System.out.println("Escribe una frase.");
 		String frase = entrada.nextLine();
-		
+
 		// Buscamos si aparecen palabrotas en la frase.
 		
+		boolean hayPalabrotas = false;
+		for(int i = 0; i < palabrotas.length; i++) {
+			if(frase.indexOf(palabrotas[i]) >= 0) {
+				System.out.println("Alerta de palabrotas");
+				hayPalabrotas = true;
+				break;
+			}
+		}
+		
+		if(!hayPalabrotas) {
+			System.out.println("La frase es correcta.");
+		}
 		
 	}
 
 }
+
+
+
+
+
+
