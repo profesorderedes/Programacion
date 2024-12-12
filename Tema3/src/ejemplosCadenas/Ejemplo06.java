4c32d8ef2b97ebc427c7b292c94ba0130198b5cb
carlos <profesorderedes@gmail.com>
2024-12-12 17:25:53 +0100

    jue 12 dic 2024 17:25:53 CET

---
+++ Tema3/src/ejemplosCadenas/Ejemplo06.java
@@ -0,0 +1,26 @@
+package ejemplosCadenas;
+
+public class Ejemplo06 {
+
+	public static void main(String[] args) {
+
+		String palabra1 = "perro";
+		String palabra2 = "gato";
+
+		System.out.println("La primera palabra, alfabéticamente, es:");
+		if (palabra1.compareTo(palabra2) < 0) {
+			System.out.println(palabra1);
+			
+		} else if (palabra1.compareTo(palabra2) > 0) {
+			System.out.println(palabra2);
+			
+		} else {
+			System.out.println("(Son la misma palabra)");
+		}
+
+	}
+
+}
+
+
+
--- Tema3/src/ejerciciosCadenas/Ejer12b.java
+++ Tema3/src/ejerciciosCadenas/Ejer12b.java
@@ -9,9 +9,8 @@ public class Ejer12b {
 
 		Scanner entrada = new Scanner(System.in);
 
-//		System.out.print("Dime una frase: ");
-//		String frase = entrada.nextLine();
-		String frase = "PROGRAMA QUE LEE UNA FRASE";
+		System.out.print("Dime una frase: ");
+		String frase = entrada.nextLine();
 
 		// Recorremos la cadena de espacio en espacio.
 		int ini = 0;
@@ -26,9 +25,40 @@ public class Ejer12b {
 		System.out.println("Hay " + (espacios + 1) + " palabras.");
 
 		String[] palabras = new String[espacios + 1];
-		
+
 		// Rellenamos el array de palabras colocando una palabra en cada posición.
-		
+
+		int fin;
+		int pos = 0;
+		ini = 0;
+
+		// Mientras quede algún espacio en la frase, seguimos mostrando palabras.
+		while (frase.indexOf(" ", ini) != -1) {
+
+			// fin será la posición del siguiente espacio
+			fin = frase.indexOf(" ", ini);
+
+			// Mostramos la palabra entre las posiciones ini y fin.
+			// System.out.println(frase.substring(ini, fin));
+			palabras[pos] = frase.substring(ini, fin);
+			pos++;
+
+			// Actualizamos ini para que sea la posición justo después del
+			// espacio.
+			ini = fin + 1;
+
+		}
+
+		// Falta por mostrar la última palabra, ya que ésta no tiene un espacio
+		// al final.
+		// System.out.println(frase.substring(ini));
+		palabras[pos] = frase.substring(ini);
+
+		// Mostramos el array.
+
+		for (int i = 0; i < palabras.length; i++) {
+			System.out.println(palabras[i]);
+		}
 
 	}
 
