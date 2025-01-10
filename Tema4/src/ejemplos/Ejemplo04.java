4b24b53301ba23f97ad44389ba01271a3586b4f9
carlos <profesorderedes@gmail.com>
2025-01-10 15:35:50 +0100

    vie 10 ene 2025 15:35:49 CET

--- Tema4/src/ejemplos/Ejemplo04.java
+++ Tema4/src/ejemplos/Ejemplo04.java
@@ -11,6 +11,8 @@ public class Ejemplo04 {
 		
 		System.out.println("El resultado ha sido " + doble(15) + ".");
 		
+		System.out.println("El triple de " + 5 + " es " + triple(5) + ".");
+		
 	}
 	
 	// Método que recibe un número y devuelve el doble del mismo.
@@ -20,5 +22,37 @@ public class Ejemplo04 {
 	}
 	
 	// triple(num)
+	static int triple(int num) {
+		
+		int triple = num * 3;
+		return triple;
+		
+	}
+	
+	// Método que recibe una palabra como argumento y devuelve la inicial.
+	// inicial(palabra) 
+	
+	// Método que recibe el radio de un círculo como argumento y devuelve el 
+	// área de ese círculo.
+	// areaCirculo(r)
+	
+	// Método que recibe el radio de una esfera como argumento y devuelve el 
+	// volumen de esa esfera.
+	// volumenEsfera(r)
+	
+	// Método que recibe un precio, el descuento a aplicar y el iva a aplicar y
+	// devuelve el precio final.
+	// precioVenta(precio, descuento, iva)
+	
+	// Método que devuelve el área de un triángulo tras recibir como parámetros
+	// su base y su altura.
+	// areaTriangulo(base, altura)
+	
+	
 
 }
+
+
+
+
+
