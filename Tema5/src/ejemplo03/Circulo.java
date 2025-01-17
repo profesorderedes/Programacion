abe2cf431da1a9b9c564c2c9d0ea8ab9dfb4e473
carlos <profesorderedes@gmail.com>
2025-01-17 17:03:45 +0100

    vie 17 ene 2025 17:03:45 CET

--- Tema5/src/ejemplo03/Circulo.java
+++ Tema5/src/ejemplo03/Circulo.java
@@ -5,11 +5,11 @@ Crear una clase Main, que sólo contendrá el método main( ).
 2. Crear una clase Circulo.
 1. Atributos: x, y, radio.
 2. Ningún método, de momento.
-3. Repetir el proceso del ejemplo anterior, creando un par de objetos y mostrando luego sus
+3. Añadir el método mostrar( ). Sustituir en la clase Main.
+4. Repetir el proceso del ejemplo anterior, creando un par de objetos y mostrando luego sus
 valores.
-4. Añadir el método mostrar( ). Sustituir en la clase Main.
-5. Añadir el método area( ).
-6. Añadir el método perimetro( ).
+5. Añadir el método area( ) (devuelve el área del círculo).
+6. Añadir el método perimetro( ) (devuelve el perímetro del círculo).
  */
 public class Circulo {
 
