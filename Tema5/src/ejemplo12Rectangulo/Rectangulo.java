4405f488784815da1cd432c68dbba130a78bd66b
carlos <profesorderedes@gmail.com>
2025-01-28 19:05:36 +0100

    mar 28 ene 2025 19:05:36 CET

--- Tema5/src/ejemplo12Rectangulo/Rectangulo.java
+++ Tema5/src/ejemplo12Rectangulo/Rectangulo.java
@@ -53,4 +53,44 @@ public class Rectangulo {
 
 	}
 
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getBase() {
+		return base;
+	}
+
+	public void setBase(int base) {
+		this.base = base;
+	}
+
+	public int getAltura() {
+		return altura;
+	}
+
+	public void setAltura(int altura) {
+		this.altura = altura;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
 }
