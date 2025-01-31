6262c29a5d7cc929a0f28acb34f6d95c6f5b85bf
carlos <profesorderedes@gmail.com>
2025-01-31 17:00:58 +0100

    vie 31 ene 2025 17:00:58 CET

--- Tema5/src/ejemplo14Mates/Main.java
+++ Tema5/src/ejemplo14Mates/Main.java
@@ -1,17 +1,14 @@
 package ejemplo14Mates;
 
 public class Main {
-	
+
 	public static void main(String[] args) {
-		
+
 		System.out.println(Mates.elevar(10, 0));
-	
+
 		int maximo = Mates.max(-8, -2);
 		System.out.println("Máximo = " + maximo);
-		
-		// Explicación de uso del array args.
-		
-		
+
 	}
 
 }
---
+++ Tema5/src/miscelanea/ArgumentosConsola.java
@@ -0,0 +1,12 @@
+package miscelanea;
+
+public class ArgumentosConsola {
+
+	public static void main(String[] args) {
+
+		// Explicación de uso del array args.
+		
+		
+	}
+
+}
