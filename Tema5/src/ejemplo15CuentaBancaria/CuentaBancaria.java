a5aff6a99fee55883fc7f3ef27d1d1f3b74580da
carlos <profesorderedes@gmail.com>
2025-02-10 20:04:16 +0100

    lun 10 feb 2025 20:04:16 CET

--- Tema5/src/ejemplo15CuentaBancaria/CuentaBancaria.java
+++ Tema5/src/ejemplo15CuentaBancaria/CuentaBancaria.java
@@ -2,31 +2,31 @@ package ejemplo15CuentaBancaria;
 
 public class CuentaBancaria {
 
-	private int saldo;
-	
-	public CuentaBancaria(int saldo) {
+	private double saldo;
+
+	public CuentaBancaria(double saldo) {
 		this.saldo = saldo;
 	}
-	
-	public void depositar(int cantidad) {
-		
+
+	public void depositar(double cantidad) {
+
 		saldo = saldo + cantidad;
-		
+
 	}
-	
-	public boolean retirar(int cantidad) {
-		
+
+	public boolean retirar(double cantidad) {
+
 		if (cantidad > saldo) {
 			return false;
 		} else {
+			saldo = saldo - cantidad;
 			return true;
 		}
-		
-		
+
 	}
-	
-	public int obtenerSaldo() {
+
+	public double obtenerSaldo() {
 		return saldo;
 	}
-	
+
 }
--- Tema5/src/ejemplo15CuentaBancaria/Main.java
+++ Tema5/src/ejemplo15CuentaBancaria/Main.java
@@ -3,15 +3,22 @@ package ejemplo15CuentaBancaria;
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		CuentaBancaria c1 = new CuentaBancaria(3000);
-		
+
 		c1.depositar(500);
-		System.out.println(c1.retirar(3000));
-		
 		System.out.println(c1.obtenerSaldo());
 		
+		boolean retirado = c1.retirar(4000);
 		
+		if(retirado) {
+			System.out.println("Se ha retirado la cantidad.");
+		}else {
+			System.out.println("No se ha podido retirar la cantidad. Fondos insuficientes.");
+		}
+		
+		System.out.println(c1.obtenerSaldo());
+
 	}
 
 }
