8e6533a8283c5f041009fcae51272e000e895d9f
carlos <profesorderedes@gmail.com>
2025-02-19 19:10:06 +0100

    mié 19 feb 2025 19:10:06 CET

--- Tema5Proyecto9/src/Inicio.java
+++ Tema5Proyecto9/src/Inicio.java
@@ -3,7 +3,11 @@ public class Inicio {
 
 	public static void main(String[] args) {
 
-		Motor motor = new Motor(100, "Samsung", "A1");
+		Motor motor1 = new Motor(100, "Samsung", "A1");
+
+		Lavadora lava1 = new Lavadora(1, motor1, "Balay", 500);
+
+		System.out.println(lava1.toString());
 
 	}
 
---
+++ Tema5Proyecto9/src/Taller.java
@@ -0,0 +1,9 @@
+
+public class Taller {
+
+	private int MAX_LAVADORAS = 20;
+	private Lavadora[] lavadoras = new Lavadora[MAX_LAVADORAS];
+
+	
+	
+}
