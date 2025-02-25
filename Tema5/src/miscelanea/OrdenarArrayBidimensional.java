87dfb271ac3852dfc68d89226f70aad696d4228c
carlos <profesorderedes@gmail.com>
2025-02-25 18:29:29 +0100

    mar 25 feb 2025 18:29:28 CET

--- Tema5/src/miscelanea/OrdenarArrayBidimensional.java
+++ Tema5/src/miscelanea/OrdenarArrayBidimensional.java
@@ -4,6 +4,13 @@ public class OrdenarArrayBidimensional {
 
 	public static void main(String[] args) {
 		
+		String[][] notas = { 
+				{ "Jose Luis Martínez", "7" }, 
+				{ "Antonia Ruiz", "9" }, 
+				{ "Marcos Ramírez", "10" },
+				{ "Juana Herranz", "4" }, 
+				{ "Alberto López", "7" } 
+			};
 		
 		
 	}
