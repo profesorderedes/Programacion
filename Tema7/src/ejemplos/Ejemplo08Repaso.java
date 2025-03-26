f03fa129fc782497859504d69943845c0538c6f0
carlos <profesorderedes@gmail.com>
2025-03-26 18:27:51 +0100

    mié 26 mar 2025 18:27:51 CET

--- Tema7/src/ejemplos/Ejemplo08Repaso.java
+++ Tema7/src/ejemplos/Ejemplo08Repaso.java
@@ -7,14 +7,16 @@ import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 public class Ejemplo08Repaso extends JFrame {
 
 	public Ejemplo08Repaso() {
+
 		super("Comentarios");
-		setSize(250, 380);
+		setSize(250, 420);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 
 		setLayout(new FlowLayout());
@@ -22,30 +24,32 @@ public class Ejemplo08Repaso extends JFrame {
 		JLabel lblNacimiento = new JLabel("Fecha de nacimiento:");
 		JTextField txtNacimiento = new JTextField(22);
 
-		add(lblNacimiento);
-		add(txtNacimiento);
-
 		JLabel lblDNI = new JLabel("DNI:");
 		JTextField txtDni = new JTextField(22);
+
+		JCheckBox chkTerminos = new JCheckBox("Aceptar términos y condiciones:");
+		JCheckBox chkSpam = new JCheckBox("Aceptar recibir spam", true);
+
+		JLabel lblQuejas = new JLabel("Quejas:");
+		JTextArea txtQuejas = new JTextArea();
+		JScrollPane panelQuejas = new JScrollPane(txtQuejas);
+
+		panelQuejas.setPreferredSize(new Dimension(220, 180));
+
+		JButton btnEnviar = new JButton("Enviar");
+		JButton btnBorrar = new JButton("Borrar");
+
+		add(lblNacimiento);
+		add(txtNacimiento);
 
 		add(lblDNI);
 		add(txtDni);
 
-		JCheckBox chkTerminos = new JCheckBox("Acceptar términos y condiciones:");
-		JCheckBox chkSpam = new JCheckBox("Acceptar recibir spam", true);
-
 		add(chkTerminos);
 		add(chkSpam);
 
-		JLabel lblQuejas = new JLabel("Quejas:");
-		JTextArea txtQuejas = new JTextArea();
-		txtQuejas.setPreferredSize(new Dimension(220, 180));
-
 		add(lblQuejas);
-		add(txtQuejas);
-
-		JButton btnEnviar = new JButton("Enviar");
-		JButton btnBorrar = new JButton("Borrar");
+		add(panelQuejas);
 
 		add(btnEnviar);
 		add(btnBorrar);
