440fea9cd0fc4a15a12e6c5f1216f22833fb23d7
carlos <profesorderedes@gmail.com>
2025-03-28 16:59:45 +0100

    vie 28 mar 2025 16:59:45 CET

---
+++ Tema7/src/ejemplos/Ejemplo12Slider.java
@@ -0,0 +1,43 @@
+package ejemplos;
+
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JSlider;
+
+public class Ejemplo12Slider extends JFrame {
+
+	public Ejemplo12Slider() {
+
+		super("Espacio de disco");
+		setSize(400, 300);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+		setLayout(new FlowLayout());
+
+		setLocationRelativeTo(null);
+
+		JLabel lblEspacio = new JLabel("Elija el espacio del disco duro (en GB):");
+
+		JSlider sldEspacio = new JSlider(JSlider.HORIZONTAL, 5, 50, 25);
+		sldEspacio.setPreferredSize(new Dimension(390, 40));
+		
+		sldEspacio.setPaintTicks(true);
+		sldEspacio.setPaintLabels(true);
+		sldEspacio.setMinorTickSpacing(1);
+		sldEspacio.setMajorTickSpacing(5);
+
+		add(lblEspacio);
+		add(sldEspacio);
+
+		setVisible(true);
+
+	}
+
+	public static void main(String[] args) {
+		new Ejemplo12Slider();
+	}
+
+}
---
+++ Tema7/src/ejemplos/Ejemplo13Iconos.java
@@ -0,0 +1,42 @@
+package ejemplos;
+
+import java.awt.FlowLayout;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Ejemplo13Iconos extends JFrame {
+
+	public Ejemplo13Iconos() {
+
+		super("Barras de herramientas");
+		setSize(500, 300);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+		setLayout(new FlowLayout(FlowLayout.LEFT));
+
+		setLocationRelativeTo(null);
+
+		// Para trabajar con una imagen, tenemos que crear un ImageIcon
+		// asociado al archivo con la imagen.
+		ImageIcon imgAbrir = new ImageIcon("iconos/openfile.gif");
+
+		// Para ver la imagen, hay que asignársela a un JLabel o un JButton.
+		JLabel lblAbrir = new JLabel(imgAbrir);
+		
+		JButton btnAbrir = new JButton("Abrir", imgAbrir);
+
+		add(lblAbrir);
+		add(btnAbrir);
+
+		setVisible(true);
+
+	}
+
+	public static void main(String[] args) {
+		new Ejemplo13Iconos();
+	}
+
+}
