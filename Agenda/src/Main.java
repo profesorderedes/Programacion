4eb45cc00cc23dcef1fc7afdd82ed2ee525de13f
carlos <profesorderedes@gmail.com>
2025-04-30 18:43:59 +0200

    mié 30 abr 2025 18:43:59 CEST

--- Agenda/src/Main.java
+++ Agenda/src/Main.java
@@ -1,3 +1,4 @@
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -22,6 +23,42 @@ public class Main {
 
 		System.out.println("\nEl contacto con id 2 está en la posición: " + miAgenda.buscarContacto(2));
 		System.out.println("El contacto con id 50 está en la posición: " + miAgenda.buscarContacto(50));
+
+		System.out.println(
+				"\n****************************************************************************************************************");
+
+		// Pruebas de getContacto().
+
+		Contacto contacto = miAgenda.getContacto(2);
+		System.out.println("\nEl contacto en la posición 2 tiene estos datos: " + contacto.toString());
+
+		contacto = miAgenda.getContacto(50);
+		System.out.println("\nEl contacto en la posición 50 tiene estos datos: " + contacto);
+
+		// Pruebas de setContacto().
+
+		Contacto contactoNuevo = new Contacto("Pérez Fraile", "Marcos", "555 11 22 77");
+		contactoNuevo.setId(10);
+
+		miAgenda.setContacto(2, contactoNuevo);
+		System.out.println("\nListado de contactos, tras cambiar el contacto en la posición 2:\n");
+		miAgenda.listarContactos();
+
+		System.out.println("\nListado de contactos, tras intentar cambiar un contacto en posiciones no válidas:\n");
+		miAgenda.setContacto(50, contactoNuevo);
+		miAgenda.setContacto(-1, contactoNuevo);
+
+		miAgenda.listarContactos();
+
+		// Pruebas de eliminarContacto().
+
+		System.out.println("\nListado de contactos, tras eliminar el contacto con id 2:\n");
+		System.out.println(miAgenda.eliminarContacto(2));
+		miAgenda.listarContactos();
+
+		System.out.println("\nListado de contactos, tras eliminar un contacto inexistente:\n");
+		System.out.println(miAgenda.eliminarContacto(20));
+		miAgenda.listarContactos();
 
 	}
 
