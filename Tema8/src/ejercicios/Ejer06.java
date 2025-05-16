71b0eec7c6086add546f626e4f2cb0412de46e24
carlos <profesorderedes@gmail.com>
2025-05-16 16:18:37 +0200

    vie 16 may 2025 16:18:37 CEST

--- Tema8/src/ejercicios/Ejer06.java
+++ Tema8/src/ejercicios/Ejer06.java
@@ -1,27 +1,11 @@
 package ejercicios;
 
 import java.awt.FlowLayout;
-
 import java.awt.event.ActionEvent;
-
 import java.awt.event.ActionListener;
 
-import java.io.BufferedReader;
-
-import java.io.File;
-
-import java.io.FileNotFoundException;
-
-import java.io.FileReader;
-
-import javax.swing.ImageIcon;
-
-import javax.swing.JButton;
-
 import javax.swing.JFileChooser;
-
 import javax.swing.JFrame;
-
 import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
