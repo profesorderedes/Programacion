4aba896c3ecf3ae0bd56078ed1e82d6f2c095559
carlos <profesorderedes@gmail.com>
2025-05-16 15:28:23 +0200

    vie 16 may 2025 15:28:23 CEST

--- Tema8/src/ejemplosArchivosTexto/Ejemplo08FileChooser.java
+++ Tema8/src/ejemplosArchivosTexto/Ejemplo08FileChooser.java
@@ -14,6 +14,9 @@ import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
@@ -29,59 +32,68 @@ public class Ejemplo08FileChooser extends JFrame {
 
 		setLocationRelativeTo(null);
 		
+		JMenuBar barraMenu = new JMenuBar();
 		
+		JMenu menuArchivo = new JMenu("Archivo");
+		
+		JMenuItem itemAbrir = new JMenuItem("Abrir");
+		menuArchivo.add(itemAbrir);
+		
+		barraMenu.add(menuArchivo);
+
 		JButton btnElegirArchivo = new JButton("Elegir un archivo");
-		btnElegirArchivo.setPreferredSize(new Dimension(150,30));
-		
+		btnElegirArchivo.setPreferredSize(new Dimension(150, 30));
+
 		JLabel lblRutaArchivo = new JLabel();
 		JTextArea txtTexto = new JTextArea();
 		JScrollPane scrTexto = new JScrollPane(txtTexto);
 		scrTexto.setPreferredSize(new Dimension(690, 420));
-		
+
 		btnElegirArchivo.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				
+
 				JFileChooser elegir = new JFileChooser();
-				
+
 				int eleccion = elegir.showOpenDialog(null);
-				
-				if(eleccion == JFileChooser.APPROVE_OPTION) {
-					
+
+				if (eleccion == JFileChooser.APPROVE_OPTION) {
+
 					System.out.println(elegir.getSelectedFile().toString());
 					File archivo = elegir.getSelectedFile();
-					
+
 					try {
 						FileReader fichero = new FileReader(archivo);
 						BufferedReader buffer = new BufferedReader(fichero);
-						
+
 						String texto = "";
 						String linea = buffer.readLine();
-						while(linea != null) {
-							
+						while (linea != null) {
+
 							texto = texto + linea + "\n";
 							linea = buffer.readLine();
-							
+
 						}
-						
+
 						txtTexto.setText(texto);
-						
+
 						buffer.close();
-						
+
 					} catch (FileNotFoundException e) {
 						e.printStackTrace();
 					} catch (IOException e) {
 						e.printStackTrace();
 					}
-					
-				}else {
+
+				} else {
 					System.out.println("Se ha cancelado.");
 				}
-				
+
 			}
 		});
 
+		setJMenuBar(barraMenu);
 		add(btnElegirArchivo);
 		add(lblRutaArchivo);
 		add(scrTexto);
