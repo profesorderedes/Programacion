5eb4e120e5dfac7a2e426e24dcd6d7ceaf9c9979
carlos <profesorderedes@gmail.com>
2025-05-30 17:08:19 +0200

    vie 30 may 2025 17:08:19 CEST

---
+++ Tema9/src/ejercicios/Ejer1.java
@@ -0,0 +1,23 @@
+package ejercicios;
+
+/*
+Crea una clase con un método:
+	String transforma(int dato)
+que transforme un número entero en base diez a binario. 
+Utiliza un procedimiento recursivo.
+ */
+public class Ejer1 {
+
+	public static void main(String[] args) {
+
+		int num = 16;
+		System.out.println("El número " + num + " en binario es " + transforma(num));
+
+	}
+
+	private static String transforma(int dato) {
+		
+		
+		return null;
+	}
+}
