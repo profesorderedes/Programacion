08b54300359aea99ce3f36eb8d714568a84edae1
carlos <profesorderedes@gmail.com>
2025-05-30 16:27:54 +0200

    vie 30 may 2025 16:27:54 CEST

---
+++ Tema9/src/ejemplos/Ejemplo10BorradoRecursivo.java
@@ -0,0 +1,37 @@
+package ejemplos;
+
+import java.io.File;
+
+public class Ejemplo10BorradoRecursivo {
+
+	public static void main(String[] args) {
+
+	}
+
+	/**
+	 * Borra el archivo o carpeta que se le pase como parámetro.
+	 *
+	 * @param archivo
+	 *
+	 *                Objeto File que representa una carpeta o archivo que se quiere
+	 *
+	 *                borrar.
+	 */
+	private static void borrar(File archivo) {
+		// Caso recursivo.
+		// El método archivo.isDirectory() devuelve true si el objeto es una
+		// carpeta.
+		if (archivo.isDirectory()) {
+			// El método archivo.listFiles() devuelve un array de objetos File
+			// que representan los archivos y carpetas contenidos en archivo.
+			for (File f : archivo.listFiles())
+				borrar(f);
+		}
+		// Caso base.
+		boolean borrado = archivo.delete();
+		if (!borrado) {
+			System.out.println("ERROR AL BORRAR");
+		}
+	}
+
+}
