2499619e8b87d23b93027c4b64237b916ea6e980
profesorderedes <profesorderedes@gmail.com>
2025-09-26 17:12:33 +0200

    Inicio del repositorio

---
+++ Tema1/src/Ejemplo05.java
@@ -0,0 +1,20 @@
+
+public class Ejemplo05 {
+
+	public static void main(String[] args) {
+
+		float pi = 3.1415926535897932F;
+
+		// Al mostrar pi se ve que no se han almacenado todos los decimales.
+		System.out.println(pi);
+
+		float numeroEnorme = 81293412394712934132F;
+
+		// Al mostrar numeroEnorme se ve que no se han almacenado todas las cifras.
+		System.out.println(numeroEnorme);
+
+		// Se ve que al usar float (y double) se pierde precisión.
+
+	}
+
+}
