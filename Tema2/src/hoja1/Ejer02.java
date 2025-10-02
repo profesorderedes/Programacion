762f7f60bceb0f89fb79da81b4aea612e27c2c9d
profesorderedes <profesorderedes@gmail.com>
2025-10-02 20:57:45 +0200

    jue 02 oct 2025 20:57:45 CEST

---
+++ Tema2/src/hoja1/Ejer02.java
@@ -0,0 +1,31 @@
+package hoja1;
+/*
+
+2.- Escribe un programa que pregunte el precio de un producto. Si vale más de 80 €, le hará un
+descuento del 10%. Se mostrará en pantalla el precio final.
+
+*/
+
+import java.util.Scanner;
+
+public class Ejer02 {
+
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.println("¿Cuál es el precio del producto?");
+		float precio = entrada.nextFloat();
+
+		if (precio > 80) {
+
+			float descuento = precio * 10 / 100;
+			precio = precio - descuento;
+		
+		}
+		
+		System.out.println("Tiene un descuento, el precio es de " + precio + "€.");
+
+	}
+
+}
