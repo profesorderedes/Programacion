06b262ccbddb81e50e43e2690dc560430b1c8088
profesorderedes <profesorderedes@gmail.com>
2025-10-13 19:22:19 +0200

    lun 13 oct 2025 19:22:19 CEST

--- Tema2/src/ejemplos/Ejemplo10While.java
+++ Tema2/src/ejemplos/Ejemplo10While.java
@@ -1,17 +1,21 @@
 package ejemplos;
 
 public class Ejemplo10While {
-	
-	public static void main(String[] args) {
-		
+
+	public static void main(String[] args) throws InterruptedException {
+
 		// Programa que escribe una cuenta atrás desde 10 hasta 1.
-		
-		int contador ;
-		while(contador ) {
+
+		int contador = 10;
+		while (contador >= 1) {
 			System.out.println(contador);
-			
+			contador--;
+
+			// Si queremos darle un retardo...
+			Thread.sleep(1000);
+
 		}
-		
+
 	}
 
 }
---
+++ Tema2/src/ejemplos/Ejemplo11While.java
@@ -0,0 +1,18 @@
+package ejemplos;
+
+public class Ejemplo11While {
+
+	public static void main(String[] args) {
+
+		// Programa que muestre los diez primeros múltiplos de 7: 7, 14, 21, 28, ...,
+		// 70.
+
+		int i = 7;
+		while (i <= 70) {
+			System.out.println(i);
+			i = i + 7;
+		}
+
+	}
+
+}
