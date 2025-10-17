1b948f6b42f7a81b0d8ac0bc8b5bafc8629e75c5
profesorderedes <profesorderedes@gmail.com>
2025-10-17 18:26:25 +0200

    vie 17 oct 2025 18:26:25 CEST

---
+++ Tema2/src/hoja2/Ejer06.java
@@ -0,0 +1,20 @@
+package hoja2;
+
+public class Ejer06 {
+
+	public static void main(String[] args) {
+
+		int i = 0;
+
+		while (i <= 200) {
+
+			System.out.println(i);
+			i = i + 2;
+
+		}
+
+		System.out.println("Fin.");
+
+	}
+
+}
---
+++ Tema2/src/hoja2/Ejer07.java
@@ -0,0 +1,23 @@
+package hoja2;
+
+public class Ejer07 {
+
+	public static void main(String[] args) {
+
+		int i = 1;
+		int j = 1;
+
+		while (j < 50) {
+
+			System.out.println(i);
+
+			i = i + 2;
+			j++;
+
+		}
+
+		System.out.println("Fin.");
+
+	}
+
+}
