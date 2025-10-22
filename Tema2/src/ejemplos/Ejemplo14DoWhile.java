ad87099677ebf8e37c9371e949c042bb227f7eaa
profesorderedes <profesorderedes@gmail.com>
2025-10-22 17:29:25 +0200

    mié 22 oct 2025 17:29:25 CEST

--- Tema2/src/ejemplos/Ejemplo14DoWhile.java
+++ Tema2/src/ejemplos/Ejemplo14DoWhile.java
@@ -5,18 +5,21 @@ import java.util.Scanner;
 public class Ejemplo14DoWhile {
 
 	public static void main(String[] args) {
-		
-		// Programa que pide un número y repita la pregunta mientras el número introducido
+
+		// Programa que pide un número y repite la pregunta mientras el número
+		// introducido
 		// no esté entre 1 y 5.
-		
+
 		Scanner entrada = new Scanner(System.in);
-		
+
+		int num;
 		do {
-			System.out.print("Escriba un número entre 1 y 5:");
-			int num = entrada.nextInt();
-		}while();
+			System.out.print("Escriba un número entre 1 y 5: ");
+			num = entrada.nextInt();
+		} while (num < 1 || num > 5);
 		
-		
+		System.out.println("Ha escrito el número " + num + ".");
+
 	}
-	
+
 }
