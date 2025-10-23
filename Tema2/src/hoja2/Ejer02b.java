06d04ba0863b266da0aac1246b71c5de12435afc
profesorderedes <profesorderedes@gmail.com>
2025-10-23 19:59:30 +0200

    jue 23 oct 2025 19:59:30 CEST

--- Tema2/src/hoja2/Ejer02b.java
+++ Tema2/src/hoja2/Ejer02b.java
@@ -8,19 +8,19 @@ import java.util.Scanner;
 public class Ejer02b {
 
 	public static void main(String[] args) {
-		
+
 		Scanner entrada = new Scanner(System.in);
-		
+
 		System.out.println("Escribe un carácter: ");
 		String caracter = entrada.nextLine();
-		
+
 		System.out.println("¿Cuántas veces quieres mostrarlo?");
 		int repeticiones = entrada.nextInt();
-		
-		for() {
+
+		for (int i = 1; i <= repeticiones; i++) {
 			System.out.println(caracter);
 		}
-		
+
 	}
 
 }
