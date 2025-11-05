5f0beda75242a23bbfe7c6bf2e79d5ad61c04ce1
profesorderedes <profesorderedes@gmail.com>
2025-11-05 17:52:33 +0100

    mié 05 nov 2025 17:52:33 CET

--- Tema3/src/ejemplos/Ejemplo01DeclararArrays.java
+++ Tema3/src/ejemplos/Ejemplo01DeclararArrays.java
@@ -13,6 +13,14 @@ public class Ejemplo01DeclararArrays {
 		// Como es un array de números, el valor por defecto de cada posición será 0.
 		double[] precios2 = new double[5];
 
+		// Accedemos a algunas posiciones del array para modificarlas.
+		precios[0] = 20; // Guardamos en la primera posición el valor 20.
+		precios[4] = 5;
+
+		// Leemos una posición y mostramos su valor.
+		System.out.println("En la posición 0 tenemos el precio " + precios[0] + " euros.");
+		System.out.println("En la posición 0 del array precios2 tenemos el valor " + precios2[0]);
+		
 	}
 
 }
---
+++ Tema3/src/ejemplos/Ejemplo02RecorrerArray.java
@@ -0,0 +1,17 @@
+package ejemplos;
+
+public class Ejemplo02RecorrerArray {
+
+	public static void main(String[] args) {
+
+		double[] precios = { 12, 102, 20.5, 50, 75 };
+
+		// Recorremos el array mostrando cada precio.
+
+		for (int i = 0; i < 5; i++) {
+			System.out.println(precios[i]);
+		}
+
+	}
+
+}
