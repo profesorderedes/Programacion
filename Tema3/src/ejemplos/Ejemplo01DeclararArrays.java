ced70726ec10795e0823033ef4626eb7ad02fdcd
profesorderedes <profesorderedes@gmail.com>
2025-11-05 17:22:32 +0100

    mié 05 nov 2025 17:22:32 CET

---
+++ Tema3/src/ejemplos/Ejemplo01DeclararArrays.java
@@ -0,0 +1,18 @@
+package ejemplos;
+
+public class Ejemplo01DeclararArrays {
+
+	public static void main(String[] args) {
+
+		// Declaramos un par de arrays.
+
+		// El primer array lo declaramos y lo inicializamos de una sola vez.
+		double[] precios = { 12, 102, 20.5, 50, 75 };
+
+		// El segundo array lo declaramos y lo inicializamos con valores por defecto.
+		// Como es un array de números, el valor por defecto de cada posición será 0.
+		double[] precios2 = new double[5];
+
+	}
+
+}
