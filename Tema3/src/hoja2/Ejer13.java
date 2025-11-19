880000ba2338e6454045026ea3f58caa25d8e3af
profesorderedes <profesorderedes@gmail.com>
2025-11-19 17:07:25 +0100

    mié 19 nov 2025 17:07:25 CET

--- Tema3/src/hoja2/Ejer13.java
+++ Tema3/src/hoja2/Ejer13.java
@@ -4,12 +4,7 @@ public class Ejer13 {
 
 	public static void main(String[] args) {
 
-		double[][] medidas = { 
-				{ 8.5, 1020, 40 }, 
-				{ 14.2, 1021, 35 },
-				{ 11, 1022, 45 }, 
-				{ 7, 1023, 50 },
-		};
+		double[][] medidas = { { 8.5, 1020, 40 }, { 14.2, 1021, 35 }, { 11, 1022, 45 }, { 7, 1023, 50 }, };
 
 		System.out.println("REGISTROS METEOROLÓGICOS");
 		System.out.println("========================");
