dfef1bae3a798c37ba0158b79d095b01f5aa555d
profesorderedes <profesorderedes@gmail.com>
2025-11-20 19:55:17 +0100

    jue 20 nov 2025 19:55:17 CET

---
+++ Tema3/src/hoja2/Ejer19b.java
@@ -0,0 +1,45 @@
+package hoja2;
+
+import java.util.Scanner;
+
+public class Ejer19b {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		String[] encabezado = { "Apellidos", "Nombre", "Sueldo" };
+		String[][] nominas = new String[4][3];
+
+		double total = 0;
+
+		for (int fila = 0; fila < nominas.length; fila++) {
+
+			for (int col = 0; col < 3; col++) {
+
+				System.out.print("Introduce el " + encabezado[col] + ": ");
+				nominas[fila][col] = sc.nextLine();
+
+			}
+
+		}
+
+		System.out.println("Tabla de nóminas:");
+
+		for (int fila = 0; fila < nominas.length; fila++) {
+
+			for (int col = 0; col < 3; col++) {
+				System.out.print(nominas[fila][col] + "\t");
+			}
+
+			System.out.println();
+
+			total = total + Integer.parseInt(nominas[fila][2]);
+
+		}
+
+		System.out.println("El total de salario que paga la empresa es de " + total + "€.");
+
+	}
+
+}
---
+++ Tema3/src/hoja2/Ejer20.java
@@ -0,0 +1,71 @@
+package hoja2;
+
+import java.util.Scanner;
+
+public class Ejer20 {
+
+	public static void main(String[] args) {
+
+		/*
+		 * 
+		 * Añade al programa anterior un bucle que busque el salario más bajo, e indique
+		 * 
+		 * el nombre y apellidos del trabajador al que corresponde, así como la cantidad
+		 * 
+		 */
+
+		Scanner entrada = new Scanner(System.in);
+
+		String[][] nominas = new String[4][3];
+
+		int suma = 0;
+		int posMin = 0;
+
+		for (int fila = 0; fila < nominas.length; fila++) {
+
+			System.out.print("Inserte los apellidos: ");
+			nominas[fila][0] = entrada.nextLine();
+
+			System.out.print("Inserte el nombre: ");
+			nominas[fila][1] = entrada.nextLine();
+
+			System.out.print("Inserte el sueldo: ");
+			nominas[fila][2] = entrada.nextLine();
+
+		}
+
+		for (int fila = 0; fila < nominas.length; fila++) {
+
+			for (int columna = 0; columna < nominas[0].length; columna++) {
+				System.out.print(nominas[fila][columna] + "\t");
+			}
+
+			System.out.println();
+
+		}
+
+		for (int fila = 0; fila < nominas.length; fila++) {
+			suma = suma + Integer.parseInt(nominas[fila][2]);
+		}
+
+		for (int fila = 0; fila < nominas.length; fila++) {
+
+			if (Integer.parseInt(nominas[fila][2]) < Integer.parseInt(nominas[posMin][2])) {
+				posMin = fila;
+			}
+
+		}
+
+		System.out.println("La suma de todas las nóminas es de: " + suma);
+
+		System.out.println("El salario más bajo pertenece a " + nominas[posMin][1] + " " 
+				+ nominas[posMin][0] + " y son " + nominas[posMin][2] + "€");
+
+	}
+
+}
+
+
+
+
+
