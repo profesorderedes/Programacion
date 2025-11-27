dfcb6a3d350389398a5d0050a6eff91fe8290b57
profesorderedes <profesorderedes@gmail.com>
2025-11-27 19:07:29 +0100

    jue 27 nov 2025 19:07:29 CET

---
+++ Tema3/src/cadenas/Ejer08c.java
@@ -0,0 +1,31 @@
+package cadenas;
+
+import java.util.Scanner;
+
+public class Ejer08c {
+
+	public static void main(String[] args) {
+
+// Programa que lee una frase desde el teclado y nos muestra
+// la primera palabra.
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Dime una frase: ");
+		String frase = input.nextLine();
+
+		String palabra = "";
+
+		// Recorremos la cadena letra a letra.
+		for (int i = 0; i < frase.indexOf(" "); i++) {
+			
+			// A la variable palabra le añadimos el caracter de la posición i.
+			palabra = palabra + frase.charAt(i);
+
+		}
+
+		System.out.println("Primera palabra: " + palabra);
+
+	}
+
+}
--- Tema3/src/cadenas/Ejer10.java
+++ Tema3/src/cadenas/Ejer10.java
@@ -11,21 +11,25 @@ public class Ejer10 {
 
 		Scanner entrada = new Scanner(System.in);
 
-		String frase = "No hay dos sin tres";
+		System.out.println("Escribe una frase y te diré cuántas palabras tiene:");
+		String frase = entrada.nextLine();
 
 		// Vamos a buscar repetidamente la cadena " ". Ya que el número de palabras va a
-		// ser
-		// el número de espacios más uno.
+		// ser el número de espacios más uno.
 
 		// Posición a partir de la cual voy a buscar el siguiente espacio.
 		int posBusqueda = 0;
 
 		int numEspacios = 0;
 
+		// Mientras queden espacios en la cadena...
 		while (frase.indexOf(" ", posBusqueda) != -1) {
 
 			numEspacios++;
 			System.out.println(frase.indexOf(" ", posBusqueda));
+
+			// Buscamos el siguiente espacio a partir de una posición después
+			// del último que hemos encontrado.
 			posBusqueda = frase.indexOf(" ", posBusqueda) + 1;
 
 		}
@@ -35,8 +39,3 @@ public class Ejer10 {
 	}
 
 }
-
-
-
-
-
