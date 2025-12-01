376d2403814f505f7bf1086bb8f78fdcd01b5473
profesorderedes <profesorderedes@gmail.com>
2025-12-01 18:51:18 +0100

    lun 01 dic 2025 18:51:18 CET

---
+++ Tema3/src/hoja3/Ejer06.java
@@ -0,0 +1,56 @@
+package hoja3;
+
+import java.util.Scanner;
+
+public class Ejer06 {
+
+	public static void main(String[] args) {
+
+		/*
+		 * Escribe un programa que lea desde consola los salarios de los 10 empleados de
+		 * un pequeña empresa y los vaya guardando en un array. A continuación el
+		 * programa calculará y mostrará por pantalla el salario mínimo, máximo y medio
+		 * de los empleados. Así como la cantidad de empleados que cobran más de 1500 €
+		 * mensuales
+		 */
+
+		Scanner entrada = new Scanner(System.in);
+
+		int[] empleados = new int[10];
+
+		int posMin = 0;
+		int posMax = 0;
+		int suma = 0;
+		int salarioAlto = 0;
+
+		for (int i = 0; i < empleados.length; i++) {
+
+			System.out.println("Inserta los sueldos de los empleados");
+			empleados[i] = entrada.nextInt();
+
+		}
+
+		for (int i = 0; i < empleados.length; i++) {
+
+			if (empleados[i] < empleados[posMin]) {
+				posMin = i;
+			} else if (empleados[i] > empleados[posMax]) {
+				posMax = i;
+			}
+
+			suma = suma + empleados[i];
+
+			if (empleados[i] > 1500) {
+				salarioAlto++;
+			}
+
+		}
+
+		System.out.println("Salario más bajo: " + empleados[posMin]);
+		System.out.println("Salario más alto: " + empleados[posMax]);
+		System.out.println("Salario medio: " + (suma / empleados.length));
+		System.out.println("¿Cuántos empleados cobran más de 1500€?: " + salarioAlto + " empleados.");
+
+	}
+
+}
