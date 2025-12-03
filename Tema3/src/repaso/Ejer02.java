f1b801cd91b8544f4c291048949ce9c7909aa82e
profesorderedes <profesorderedes@gmail.com>
2025-12-03 18:20:43 +0100

    mié 03 dic 2025 18:20:43 CET

--- Tema3/src/repaso/Ejer02.java
+++ Tema3/src/repaso/Ejer02.java
@@ -9,27 +9,34 @@ package repaso;
 	f) Ordena el array por la nota de la tercera evaluación.
 */
 public class Ejer02 {
-	
+
 	public static void main(String[] args) {
-		
+
 		// Estructura del array:
 		// id_alumno, nota 1ª eval, nota 2ª eval, nota 3ª eval
-		int[][] notas = {
-				{1, 9, 7, 8},
-				{2, 4, 5, 6},
-				{3, 8, 5, 5},
-				{4, 10, 9, 10},
-				{5, 6, 3, 4},
-				{6, 6, 5, 9},
+		int[][] notas = { 
+				{ 1, 9, 7, 8 }, 
+				{ 2, 4, 5, 6 }, 
+				{ 3, 8, 5, 5 }, 
+				{ 4, 10, 9, 10 }, 
+				{ 5, 6, 3, 4 },
+				{ 6, 6, 5, 9 }, 
 		};
-		
+
 		// Muestra el array en forma de tabla.
 		
-		double num = 10.67261;
 		
-		System.out.println(Math.round(num * 10)/10.0);
-		
-		
+
+		// Saca la nota media de las tres evaluaciones de cada alumno.
+
+		// Indica el alumno con mejor nota en la primera evaluación.
+
+		// Cuenta cuántos alumnos han suspendido alguna evaluación.
+
+		// Cuenta cuántas evaluaciones suspendidas hay.
+
+		// Ordena el array por la nota de la tercera evaluación.
+
 	}
 
 }
