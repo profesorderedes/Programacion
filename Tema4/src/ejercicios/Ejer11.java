0834951f7d3073bd8adbbe9a47c68595d76c8835
profesorderedes <profesorderedes@gmail.com>
2025-12-18 19:27:18 +0100

    jue 18 dic 2025 19:27:18 CET

---
+++ Tema4/src/ejercicios/Ejer11.java
@@ -0,0 +1,28 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class Ejer11 {
+
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.println("Introduce el año: ");
+		int anyo = entrada.nextInt();
+
+		if (esBisiesto(anyo)) {
+			System.out.println(anyo + " es bisiesto.");
+		} else {
+			System.out.println(anyo + " no es bisiesto.");
+		}
+
+	}
+
+	static boolean esBisiesto(int año) {
+
+		return (año % 4 == 0 && año % 100 != 0) || (año % 400 == 0);
+
+	}
+
+}
