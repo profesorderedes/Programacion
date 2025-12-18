68aca4385cf1344ece6beef66efe081675d61a59
profesorderedes <profesorderedes@gmail.com>
2025-12-18 18:44:35 +0100

    jue 18 dic 2025 18:44:35 CET

---
+++ Tema4/src/ejercicios/Ejer07.java
@@ -0,0 +1,24 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class Ejer07 {
+
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.println("Dame una temperatura en grados fahrenheit: ");
+		double temp = entrada.nextDouble();
+
+		System.out.println(temp + "ºF en centígrados es " + centigrados(temp) + "ºC.");
+
+	}
+
+	static double centigrados(double temp) {
+
+		return (temp - 32) * 5 / 9;
+
+	}
+
+}
