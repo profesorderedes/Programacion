e2058d9b716d70416de3ea355b90afaa9a475080
profesorderedes <profesorderedes@gmail.com>
2026-01-07 18:55:34 +0100

    mié 07 ene 2026 18:55:34 CET

--- Tema4/src/ejercicios/Ejer12.java
+++ Tema4/src/ejercicios/Ejer12.java
@@ -1,17 +1,48 @@
 package ejercicios;
 
+import java.util.Arrays;
+
 public class Ejer12 {
-	
+
 	public static void main(String[] args) {
-		
-		
-		
+
+		int[] edades = { 85, 30, 92, 95, 40, 26 };
+
+		arrayOrdenado(edades);
+
 	}
-	
+
 	static void arrayOrdenado(int[] numeros) {
-		
-		
-		
+
+		int posMin, aux;
+
+		for (int i = 0; i < numeros.length - 1; i++) {
+
+			posMin = i;
+
+			// Buscamos la posición del menor número entre i y el final.
+			for (int j = i + 1; j < numeros.length; j++) {
+				if (numeros[j] < numeros[posMin]) {
+					posMin = j;
+				}
+			}
+
+			// Intercambiamos el menor con la posición i.
+			aux = numeros[i];
+			numeros[i] = numeros[posMin];
+			numeros[posMin] = aux;
+
+		}
+
+		System.out.println(Arrays.toString(numeros));
+
 	}
 
 }
+
+
+
+
+
+
+
