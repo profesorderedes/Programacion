dca1de84d850e1f11f93fcece567a336783c865e
profesorderedes <profesorderedes@gmail.com>
2026-01-19 18:41:16 +0100

    lun 19 ene 2026 18:41:15 CET

--- Utilidades/src/utilidades/Consola.java
+++ Utilidades/src/utilidades/Consola.java
@@ -1,5 +1,94 @@
 package utilidades;
 
+import java.util.Scanner;
+
 public class Consola {
 
+	private static Scanner entrada = new Scanner(System.in);
+
+	public static int leerInt(String mensajeError) {
+
+		while (!entrada.hasNextInt()) {
+
+			System.out.println(mensajeError);
+			entrada.nextLine();
+
+		}
+
+		int num = entrada.nextInt();
+
+		entrada.nextLine();
+
+		return num;
+	}
+
+	public static int leerInt() {
+
+		return leerInt("Entrada incorrecta: se esperaba un número entero.");
+	}
+
+	public static int leerInt(String mensajeError, int min, int max) {
+
+		int num;
+
+		do {
+
+			num = leerInt(mensajeError);
+
+			if (num < min || num > max) {
+				System.out.println("El número no puede ser menor que " + min + " ni mayor que " + max + ".");
+			}
+
+		} while (num < min || num > max);
+
+		return num;
+	}
+
+	public static double leerDouble(String mensajeError) {
+
+		while (true) {
+
+			if (!entrada.hasNextDouble()) {
+
+				System.out.println(mensajeError);
+				entrada.nextLine();
+			} else {
+
+				double num = entrada.nextDouble();
+				entrada.nextLine();
+				return num;
+			}
+		}
+	}
+
+	public static double leerDouble() {
+
+		return leerDouble("Entrada incorrecta: se esperaba un número.");
+	}
+
+	public static String leerString() {
+		
+		return entrada.nextLine();
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
---
+++ Utilidades/src/utilidades/Pruebas.java
@@ -0,0 +1,15 @@
+package utilidades;
+
+public class Pruebas {
+
+	public static void main(String[] args) {
+
+		System.out.println("Escribe un número entero: ");
+		double numero = Consola.leerDouble();
+
+		System.out.println("Escribe otro número entero: ");
+		numero = Consola.leerDouble();
+
+	}
+
+}
