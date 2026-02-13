be3f547824c3cf706f93f390ddaf5b17945e2c37
profesorderedes <profesorderedes@gmail.com>
2026-02-13 18:32:50 +0100

    vie 13 feb 2026 18:32:50 CET

--- Tema5/src/prac1/Main.java
+++ Tema5/src/prac1/Main.java
@@ -6,16 +6,20 @@ public class Main {
 
 //		Medio me1 = new Medio();
 
-		Mp3 mp1 = new Mp3();
+		Mp3 mp1 = new Mp3("Highway to hell", 240, "AC/DC", "Rock", 2);
 
-		Ogg o1 = new Ogg();
+		Ogg o1 = new Ogg("Highway to hell", 240, "AC/DC", "Rock", 2);
 
 		mp1.reproducir();
 		o1.reproducir();
 
-		Medio m1 = new Mp3();
+		Medio m1 = new Mp3("Highway to hell", 240, "AC/DC", "Rock", 2);
 		m1.reproducir();
-		m1.guardar();
+//		m1.guardar();
+
+		((Guardable) m1).guardar();
+
+		Guardable g1 = new Ogg("Highway to hell", 240, "AC/DC", "Rock", 2);
 
 	}
 
--- Tema5/src/prac1/Mp3.java
+++ Tema5/src/prac1/Mp3.java
@@ -4,6 +4,11 @@ public class Mp3 extends Musica implements Guardable {
 
 	private int modo;
 
+	public Mp3(String nombre, double duracion, String artista, String estilo, int modo) {
+		super(nombre, duracion, artista, estilo);
+		this.modo = modo;
+	}
+
 	@Override
 	public boolean guardar() {
 		System.out.println("Guardando...");
