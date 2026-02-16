a79727b25c7ed77673d2b6724c72a206707f12db
profesorderedes <profesorderedes@gmail.com>
2026-02-16 20:53:51 +0100

    lun 16 feb 2026 20:53:51 CET

--- Tema6/src/ejemplos/Ejemplo04.java
+++ Tema6/src/ejemplos/Ejemplo04.java
@@ -6,26 +6,26 @@ import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-public class Ejemplo04 extends JFrame{
+public class Ejemplo04 extends JFrame {
 
 	public Ejemplo04() {
-		
+
 		super("Hobbies");
-		setSize(200,200);
+		setSize(200, 200);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
+
 		FlowLayout layout = new FlowLayout(FlowLayout.LEFT);
 		setLayout(layout);
-		
+
 		JLabel lblHobbies = new JLabel("Elije tus hobbies favoritos:");
-		
+
 		JCheckBox chkLeer = new JCheckBox("Leer", true);
 		JCheckBox chkJugar = new JCheckBox("Jugar");
 		JCheckBox chkVerSeries = new JCheckBox("Ver series");
 		JCheckBox chkDibujar = new JCheckBox("Dibujar");
 		JCheckBox chkViajar = new JCheckBox("Viajar");
 		JCheckBox chkConducir = new JCheckBox("Conducir");
-		
+
 		add(lblHobbies);
 		add(chkLeer);
 		add(chkJugar);
@@ -33,11 +33,11 @@ public class Ejemplo04 extends JFrame{
 		add(chkDibujar);
 		add(chkViajar);
 		add(chkConducir);
-		
+
 		setVisible(true);
-		
+
 	}
-	
+
 	public static void main(String[] args) {
 		Ejemplo04 ventana = new Ejemplo04();
 	}
