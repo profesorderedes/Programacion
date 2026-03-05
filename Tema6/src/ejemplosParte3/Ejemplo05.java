0f27017b76e4ad3ca09cc9615140cd20ec7ffc8c
profesorderedes <profesorderedes@gmail.com>
2026-03-05 19:38:09 +0100

    jue 05 mar 2026 19:38:09 CET

---
+++ Tema6/src/ejemplosParte3/Ejemplo05.java
@@ -0,0 +1,56 @@
+package ejemplosParte3;
+
+import java.awt.FlowLayout;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Ejemplo05 extends JFrame implements ItemListener {
+
+	private JComboBox<String> cmbProvincias;
+	private JLabel lblProvincia;
+
+	public Ejemplo05() {
+
+		super("Provincias");
+		setSize(400, 300);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+		setLayout(new FlowLayout(FlowLayout.LEFT));
+
+		setLocationRelativeTo(null);
+
+		String[] provincias = { "Baleares", "Alicante", "Murcia", "Jaén", "Albacete" };
+
+		cmbProvincias = new JComboBox<String>(provincias);
+		cmbProvincias.addItemListener(this);
+
+		lblProvincia = new JLabel("Seleccionada: Baleares");
+
+		add(cmbProvincias);
+		add(lblProvincia);
+
+		setVisible(true);
+
+	}
+
+	public static void main(String[] args) {
+		new Ejemplo05();
+	}
+
+	@Override
+	public void itemStateChanged(ItemEvent e) {
+
+		String provincia = (String) cmbProvincias.getSelectedItem();
+
+		// Forma alternativa:
+		// String provincia = provincias[cmbProvincias.getSelectedIndex()];
+
+		lblProvincia.setText(provincia);
+
+	}
+
+}
