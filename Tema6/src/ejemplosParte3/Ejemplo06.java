03bbfdfe27f69c0106454bb5f4531aa2dded7f40
profesorderedes <profesorderedes@gmail.com>
2026-03-09 20:58:29 +0100

    lun 09 mar 2026 20:58:29 CET

---
+++ Tema6/src/ejemplosParte3/Ejemplo06.java
@@ -0,0 +1,72 @@
+package ejemplosParte3;
+
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Ejemplo06 extends JFrame implements KeyListener {
+
+	private JLabel lblMensaje;
+
+	public Ejemplo06() {
+
+		super("KeyListener");
+		setSize(400, 300);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+		setLayout(new FlowLayout(FlowLayout.LEFT));
+
+		setLocationRelativeTo(null);
+
+		lblMensaje = new JLabel("Pulsa alguna tecla...");
+		lblMensaje.setFont(new Font("Arial", Font.PLAIN, 24));
+
+		add(lblMensaje);
+
+		addKeyListener(this);
+
+		setVisible(true);
+
+	}
+
+	public static void main(String[] args) {
+		new Ejemplo06();
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+
+		// Pasamos la letra a minúsculas.
+		char letra = e.getKeyChar();
+		String cadena = letra + "";
+		cadena = cadena.toLowerCase();
+		letra = cadena.charAt(0);
+		
+		// Según qué letra sea, indicamos un movimiento.
+		if (letra == 'w' ) {
+			lblMensaje.setText("Arriba");
+		} else if (letra == 's') {
+			lblMensaje.setText("Abajo");
+		} else if (letra == 'a') {
+			lblMensaje.setText("Izquierda");
+		} else if (letra == 'd') {
+			lblMensaje.setText("Derecha");
+		}
+
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+
+	}
+
+}
---
+++ Tema6/src/ejemplosParte3/Ejemplo07.java
@@ -0,0 +1,65 @@
+package ejemplosParte3;
+
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Ejemplo07 extends JFrame implements KeyListener {
+
+	private JLabel lblMensaje;
+
+	public Ejemplo07() {
+
+		super("KeyListener");
+		setSize(400, 300);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+		setLayout(new FlowLayout(FlowLayout.LEFT));
+
+		setLocationRelativeTo(null);
+
+		lblMensaje = new JLabel("Pulsa alguna tecla...");
+		lblMensaje.setFont(new Font("Arial", Font.PLAIN, 24));
+
+		add(lblMensaje);
+
+		addKeyListener(this);
+
+		setVisible(true);
+
+	}
+
+	public static void main(String[] args) {
+		new Ejemplo07();
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		System.out.println("Pulsado");
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+
+		if (e.getKeyCode() == KeyEvent.VK_UP) {
+			lblMensaje.setText("Arriba");
+		} else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+			lblMensaje.setText("Abajo");
+		} else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+			lblMensaje.setText("Izquierda");
+		} else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			lblMensaje.setText("Derecha");
+		}
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+
+	}
+
+}
