64f87d1c5964003d1606ca191e7f8e1dc340f1d3
profesorderedes <profesorderedes@gmail.com>
2026-05-04 15:23:12 +0200

    lun 04 may 2026 15:23:12 CEST

---
+++ Tema8/src/main/java/ejercicios/Ejercicio04.java
@@ -0,0 +1,48 @@
+package ejercicios;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Ejercicio04 {
+
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.print("Título de la página: ");
+		String title = entrada.nextLine();
+
+		System.out.print("Encabezado de la página: ");
+		String h1 = entrada.nextLine();
+
+		System.out.print("Contenido de la página: ");
+		String p = entrada.nextLine();
+
+		if (crearArchivoHTML(title, h1, p)) {
+			System.out.println("Se ha creado la página en tu carpeta personal. De nada.");
+		}
+	}
+
+	private static boolean crearArchivoHTML(String title, String h1, String p) {
+
+		try {
+			BufferedWriter html = new BufferedWriter(new FileWriter(System.getProperty("user.home") + "/index.html"));
+
+			html.write(
+					"<!DOCTYPE html>\n<html lang=\"es\">\n\n<head>\n\t<meta charset=\"UTF-8\">\n\t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n\t<title>"
+							+ title + "</title>\n</head>\n\n<body>\n\t<h1>" + h1 + "</h1>\n\t<p>" + "\n\t\t" + p
+							+ "\n\t</p>\n</body>\n\n</html>");
+
+			html.close();
+			return true;
+
+		} catch (IOException e) {
+			System.out.println("Error al crear el archivo HTML");
+			return false;
+		}
+
+	}
+
+}
